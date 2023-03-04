d77e804765171920b09a9d917f48429c318d4275
Fynn <fynn_kroeger@web.de>
2023-03-04 13:27:32 +0100

    refactor history, introduce ExecutedStep dataclass

--- protzilla/history.py
+++ protzilla/history.py
@@ -1,60 +1,55 @@
 from pathlib import Path
-from typing import List, Any
-
+from dataclasses import dataclass
 import pandas as pd
-
-# TODO put constants in constants-file
-PATH_TO_RUNS = "user_data/runs/"
+from .constants.constants import PATH_TO_RUNS
 
 
 class History:
-
     def __init__(self, run_name, df_mode="disk"):
         assert df_mode in ("disk", "memory")
 
         self.df_mode = df_mode
         self.run_name = run_name
-        # list of dicts
-        self.steps: list[dict] = []
-        pass
+        self.steps: list[ExecutedStep] = []
 
-    def get_df(self, index):
+    def add_step(
+        self,
+        section,
+        step,
+        method,
+        parameters,
+        dataframe: pd.DataFrame,
+        outputs: dict,
+        plots,
+    ):
         if self.df_mode == "disk":
-            return pd.read_csv(self.df_path(index))
-        if self.df_mode == "memory":
-            return self.steps[index]["output"]["df"]
-
-    def set_df(self, index, df):
-        if self.df_mode == "disk":
-            df.to_csv(self.df_path(index))
-        if self.df_mode == "memory":
-            self.steps[index]["output"]["df"] = df
-
-    def add_step(self, section_name, step_name, method_name, params: list, plots: list, output_df: pd.DataFrame,
-                 output_dict: dict):
-
-        step_dict = {
-            "section": section_name,
-            "step": step_name,
-            "method": method_name,
-            "params": params,
-            "plots": plots,
-            "output": {
-                "df": None,
-                "dict": output_dict
-            }
-        }
-
-        index = len(self.steps)
-        if self.df_mode == "disk":
-            output_df.to_csv(self.df_path(index))
-        if self.df_mode == "memory":
-            step_dict["output"]["df"] = output_df
-
-        self.steps.append(step_dict)
+            index = len(self.steps)
+            (PATH_TO_RUNS / self.run_name).mkdir(parents=True, exist_ok=True)
+            dataframe.to_csv(self.df_path(index), index=False)
+            path_or_df = self.df_path(index)
+        else:
+            path_or_df = dataframe
+        executed_step = ExecutedStep(
+            section, step, method, parameters, path_or_df, outputs, plots
+        )
+        self.steps.append(executed_step)
 
     def df_path(self, index):
-        return Path(PATH_TO_RUNS + self.run_name + "/df_" + index + ".csv")
+        return PATH_TO_RUNS / self.run_name / f"df_{index}.csv"
 
-    def name_to_indexes(self, name):
-        return [self.steps.index(step) for step in self.steps if step["step"] == name]
+
+@dataclass
+class ExecutedStep:
+    section: str
+    step: str
+    method: str
+    parameters: dict
+    _dataframe: pd.DataFrame | Path
+    outputs: dict
+    plots: list
+
+    @property
+    def dataframe(self):
+        if isinstance(self._dataframe, Path):
+            return pd.read_csv(self._dataframe)
+        return self._dataframe
--- tests/test_history.py
+++ tests/test_history.py
@@ -2,33 +2,27 @@ from protzilla.history import History
 import pandas as pd
 
 
-
-def test_history_memory():
-    # TODO split in many tests
-    hist = History("test_run", df_mode="memory")
+def test_history_memory_identity():
+    history = History("test_memory_identity", df_mode="memory")
     df1 = pd.DataFrame()
     df2 = pd.DataFrame()
-
-    hist.add_step("section1", "step1", "method1", ["param1"], [], df1, {})
-    hist.add_step("section2", "step2", "method2", ["param2"], [], df2, {})
-    print(hist.steps)
-    assert hist.get_df(0) is df1
-
-    assert hist.get_df(1) is not df1
-    hist.set_df(1, df1)
-    assert hist.get_df(1) is df1
+    history.add_step(
+        "section1", "step1", "method1", {"param1": 3}, df1, outputs={}, plots=[]
+    )
+    history.add_step(
+        "section2", "step2", "method2", {"param1": 5}, df2, outputs={}, plots=[]
+    )
+    assert history.steps[0].dataframe is df1
+    assert history.steps[0].dataframe is not df2
+    assert history.steps[1].dataframe is df2
+    assert history.steps[1].dataframe is not df1
 
 
 def test_history_disk():
-    assert True
-
-
-def test_name_to_index():
-    hist = History("test_run", df_mode="memory")
-    df1 = pd.DataFrame()
-    df2 = pd.DataFrame()
-
-    hist.add_step("section1", "step1", "method1", ["param1"], [], df1, {})
-    hist.add_step("section2", "step2", "method2", ["param2"], [], df2, {})
-
-    assert hist.name_to_indexes("step2") == [1]
+    history = History("test_disk", df_mode="disk")
+    df1 = pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4]})
+    history.add_step(
+        "section1", "step1", "method1", {"param1": 3}, df1, outputs={}, plots=[]
+    )
+    assert not isinstance(history.steps[0]._dataframe, pd.DataFrame)
+    assert history.steps[0].dataframe.equals(df1)
