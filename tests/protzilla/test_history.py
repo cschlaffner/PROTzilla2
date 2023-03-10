d4dc1ac1098886b59b603f3d9061a9a810e42369
fynnkroeger <37335646+fynnkroeger@users.noreply.github.com>
2023-03-10 15:12:33 +0100

    Implement History (#13)
    * history, test fixes
    
    * refactor history, introduce ExecutedStep dataclass
    
    * start using history in run, think about CLI
    
    * add max_quant_import
    
    * expand CLI example
    
    * add History.pop_step, disk_memory mode
    
    * added back function to run
    
    * add docstrings for history
    
    * implement review suggestions
    
    * use proteinGroups_small_cut.txt
    
    ---------
    
    Co-authored-by: brokkoli71 <brokkoli71b@gmail.com>

---
+++ tests/protzilla/test_history.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+from protzilla.history import History
+
+
+def test_history_memory_identity():
+    history = History("test_memory_identity", df_mode="memory")
+    df1 = pd.DataFrame()
+    df2 = pd.DataFrame()
+    history.add_step(
+        "section1", "step1", "method1", {"param1": 3}, df1, outputs={}, plots=[]
+    )
+    history.add_step(
+        "section2", "step2", "method2", {"param1": 5}, df2, outputs={}, plots=[]
+    )
+    assert history.steps[0].dataframe is df1
+    assert history.steps[0].dataframe is not df2
+    assert history.steps[1].dataframe is df2
+    assert history.steps[1].dataframe is not df1
+
+
+def test_history_disk():
+    history = History("test_disk", df_mode="disk")
+    df1 = pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4]})
+    history.add_step(
+        "section1", "step1", "method1", {"param1": 3}, df1, outputs={}, plots=[]
+    )
+    # the history should not contain a dataframe in memory when using disk mode
+    assert history.steps[0]._dataframe is None
+    # the dataframe should be loaded from disk correctly when using .dataframe
+    assert history.steps[0].dataframe.equals(df1)
+
+
+def test_history_disk_delete():
+    history = History("test_disk_delete", df_mode="disk")
+    df1 = pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4]})
+    history.add_step(
+        "section1", "step1", "method1", {"param1": 3}, df1, outputs={}, plots=[]
+    )
+    assert history.df_path(0).exists()
+    history.remove_step()
+    assert not history.df_path(0).exists()
---
+++ tests/protzilla/test_run.py
@@ -0,0 +1,52 @@
+from shutil import rmtree
+
+from protzilla import data_preprocessing
+from protzilla.constants.constants import PATH_TO_PROJECT, PATH_TO_RUNS
+from protzilla.importing import main_data_import
+from protzilla.run import Run
+
+
+def test_run_create():
+    # here the run should be used like in the CLI
+    rmtree(PATH_TO_RUNS / "test_run")
+    run = Run.create("test_run")
+    run.calculate_and_next(
+        main_data_import.max_quant_import,
+        file=PATH_TO_PROJECT / "tests/proteinGroups_small_cut.txt",
+        intensity_name="Intensity",
+    )
+    run.calculate_and_next(
+        data_preprocessing.filter_proteins.by_low_frequency, threshold=1
+    )
+    run.calculate_and_next(
+        data_preprocessing.filter_samples.by_protein_intensity_sum, threshold=1
+    )
+    # print([s.outputs for s in run.history.steps])
+    # to get a history that can be used to create a worklow, the section, step, method
+    # should be set by calculate_and_next
+
+
+def test_run_back():
+    rmtree(PATH_TO_RUNS / "test_run_back", ignore_errors=True)
+    run = Run.create("test_run_back")
+    run.calculate_and_next(
+        main_data_import.max_quant_import,
+        file=PATH_TO_PROJECT / "tests/proteinGroups_small_cut.txt",
+        intensity_name="Intensity",
+    )
+    df1 = run.df
+    run.calculate_and_next(
+        data_preprocessing.filter_proteins.by_low_frequency, threshold=1
+    )
+    df2 = run.df
+    assert not df1.equals(df2)
+    run.back_step()
+    assert run.df.equals(df1)
+    run.back_step()
+    assert run.df is None
+
+
+# think more about different run interfaces
+# CLI: steps, complete workflow
+# UI: location, back+next, workflow defaults
+# different classes?
