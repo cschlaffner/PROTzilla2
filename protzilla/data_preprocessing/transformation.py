19375922c9fb60359735f9ef43ca18cbfc846a23
brokkoli71 <brokkoli71b@gmail.com>
2023-03-13 18:49:50 +0100

    added log_transformation

---
+++ protzilla/data_preprocessing/transformation.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+from protzilla.data_preprocessing.plots import create_box_plots, create_histograms
+
+
+def by_log(intensity_df: pd.DataFrame, log_method="log10"):
+    """
+            A class of a transformer to log-transform intensity
+            DataFrames. Supports log-transformation to the base
+            of 2 or 10.
+
+            :param intensity_df: a protein data frame in long format
+            :type intensity_df: pd.DataFrame
+            :param log_method: String of the used log method "log10" (base 10)\
+            or "log2" (base 2). Default: "log10"
+            :type log_method: Str
+            :return: returns a pandas DataFrame in typical protzilla\
+            long format with the transformed data and an empty dict.
+            :rtype: Tuple[pandas DataFrame, dict]
+            """
+    intensity_name = intensity_df.columns.values.tolist()[3]
+    transformed_df = intensity_df.copy()
+    if log_method == "log2":
+        transformed_df[intensity_name] = np.log2(
+            transformed_df[intensity_name]
+        )
+    elif log_method == "log10":
+        transformed_df[intensity_name] = np.log10(
+            transformed_df[intensity_name]
+        )
+    else:
+        raise ValueError(
+            """Unknown log method. Known log methods are
+                         "log2" and "log10"."""
+        )
+    return transformed_df, dict()
+
+def by_log_plot(df, result_df, current_out, graph_type, group_by):
+    return _build_box_hist_plot(df, result_df, current_out, graph_type, group_by)
+
+def _build_box_hist_plot(df, result_df, current_out, graph_type, group_by):
+    if graph_type == "Boxplot":
+        fig = create_box_plots(
+            dataframe_a=df,
+            dataframe_b=result_df,
+            name_a="Before Transformation",
+            name_b="After Transformation",
+            heading="Distribution of Protein Intensities",
+            x_title="",
+            y_title="",
+            group_by=group_by,
+        )
+    if graph_type == "Histogram":
+        fig = create_histograms(
+            dataframe_a=df,
+            dataframe_b=result_df,
+            name_a="Before Transformation",
+            name_b="After Transformation",
+            heading="Distribution of Protein Intensities",
+            x_title="",
+            y_title="",
+        )
+    return [fig]
---
+++ tests/protzilla/data_preprocessing/test_transformation.py
@@ -0,0 +1,145 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from protzilla.data_preprocessing.transformation import (
+    by_log, by_log_plot
+)
+
+
+@pytest.fixture
+def log2_transformation_df():
+    test_intensity_list = (
+        ["Sample1", "Protein1", "Gene1", 2],
+        ["Sample1", "Protein2", "Gene2", 4],
+        ["Sample1", "Protein3", "Gene3", 8],
+        ["Sample1", "Protein4", "Gene4", 16],
+        ["Sample2", "Protein1", "Gene1", 4],
+        ["Sample2", "Protein2", "Gene2", np.nan],
+        ["Sample2", "Protein3", "Gene3", 4],
+        ["Sample2", "Protein4", "Gene4", 4],
+        ["Sample3", "Protein1", "Gene1", 8],
+        ["Sample3", "Protein2", "Gene2", 8],
+        ["Sample3", "Protein3", "Gene3", 8],
+        ["Sample3", "Protein4", "Gene4", 8],
+        ["Sample4", "Protein1", "Gene1", 1024],
+        ["Sample4", "Protein2", "Gene2", 1024],
+        ["Sample4", "Protein3", "Gene3", 1024],
+        ["Sample4", "Protein4", "Gene4", 1024],
+    )
+    return pd.DataFrame(
+        data=test_intensity_list,
+        columns=["Sample", "Protein ID", "Gene", "Intensity"],
+    )
+
+
+@pytest.fixture
+def log2_transformation_expected_df():
+    assertion_intensity_list = (
+        ["Sample1", "Protein1", "Gene1", 1.0],
+        ["Sample1", "Protein2", "Gene2", 2.0],
+        ["Sample1", "Protein3", "Gene3", 3.0],
+        ["Sample1", "Protein4", "Gene4", 4.0],
+        ["Sample2", "Protein1", "Gene1", 2.0],
+        ["Sample2", "Protein2", "Gene2", np.nan],
+        ["Sample2", "Protein3", "Gene3", 2.0],
+        ["Sample2", "Protein4", "Gene4", 2.0],
+        ["Sample3", "Protein1", "Gene1", 3.0],
+        ["Sample3", "Protein2", "Gene2", 3.0],
+        ["Sample3", "Protein3", "Gene3", 3.0],
+        ["Sample3", "Protein4", "Gene4", 3.0],
+        ["Sample4", "Protein1", "Gene1", 10.0],
+        ["Sample4", "Protein2", "Gene2", 10.0],
+        ["Sample4", "Protein3", "Gene3", 10.0],
+        ["Sample4", "Protein4", "Gene4", 10.0],
+    )
+    return pd.DataFrame(
+        data=assertion_intensity_list,
+        columns=["Sample", "Protein ID", "Gene", "Intensity"],
+    )
+
+
+@pytest.fixture
+def log10_transformation_df():
+    test_intensity_list = (
+        ["Sample1", "Protein1", "Gene1", 1],
+        ["Sample1", "Protein2", "Gene2", 10],
+        ["Sample1", "Protein3", "Gene3", 100],
+        ["Sample1", "Protein4", "Gene4", 1000],
+        ["Sample2", "Protein1", "Gene1", np.nan],
+        ["Sample2", "Protein2", "Gene2", 100],
+        ["Sample2", "Protein3", "Gene3", 100],
+        ["Sample2", "Protein4", "Gene4", 100],
+        ["Sample3", "Protein1", "Gene1", 10000],
+        ["Sample3", "Protein2", "Gene2", 10000],
+        ["Sample3", "Protein3", "Gene3", 10000],
+        ["Sample3", "Protein4", "Gene4", 10000],
+        ["Sample4", "Protein1", "Gene1", 100000],
+        ["Sample4", "Protein2", "Gene2", 100000],
+        ["Sample4", "Protein3", "Gene3", 100000],
+        ["Sample4", "Protein4", "Gene4", 100000],
+    )
+    return pd.DataFrame(
+        data=test_intensity_list,
+        columns=["Sample", "Protein ID", "Gene", "Intensity"],
+    )
+
+
+@pytest.fixture
+def log10_transformation_expected_df():
+    assertion_intensity_list = (
+        ["Sample1", "Protein1", "Gene1", 0.0],
+        ["Sample1", "Protein2", "Gene2", 1.0],
+        ["Sample1", "Protein3", "Gene3", 2.0],
+        ["Sample1", "Protein4", "Gene4", 3.0],
+        ["Sample2", "Protein1", "Gene1", np.nan],
+        ["Sample2", "Protein2", "Gene2", 2.0],
+        ["Sample2", "Protein3", "Gene3", 2.0],
+        ["Sample2", "Protein4", "Gene4", 2.0],
+        ["Sample3", "Protein1", "Gene1", 4.0],
+        ["Sample3", "Protein2", "Gene2", 4.0],
+        ["Sample3", "Protein3", "Gene3", 4.0],
+        ["Sample3", "Protein4", "Gene4", 4.0],
+        ["Sample4", "Protein1", "Gene1", 5.0],
+        ["Sample4", "Protein2", "Gene2", 5.0],
+        ["Sample4", "Protein3", "Gene3", 5.0],
+        ["Sample4", "Protein4", "Gene4", 5.0],
+    )
+    return pd.DataFrame(
+        data=assertion_intensity_list,
+        columns=["Sample", "Protein ID", "Gene", "Intensity"],
+    )
+
+
+def test_log2_transformation(show_figures, log2_transformation_df, log2_transformation_expected_df):
+    transformation_result = by_log(
+        log2_transformation_df, log_method="log2"
+    )[0]
+
+    if show_figures:
+        fig = by_log_plot(
+            log2_transformation_df, group_by="Protein ID"
+        )
+        fig.show()
+
+    assert transformation_result.equals(
+        log2_transformation_expected_df
+    ), f"The results of the transformation: {transformation_result} \
+            are not equal to the expected result: {log2_transformation_expected_df}"
+
+
+def test_log10_transformation(show_figures, log10_transformation_df, log10_transformation_expected_df):
+    transformation_result = by_log(
+        log10_transformation_df, log_method="log10"
+    )[0]
+
+    if show_figures:
+        fig = by_log_plot(
+            log10_transformation_df, group_by="Protein ID"
+        )
+        fig.show()
+
+    assert transformation_result.equals(
+        log10_transformation_expected_df
+    ), f"The results of the transformation: {transformation_result} \
+            are not equal to the expected result: {log10_transformation_expected_df}"
