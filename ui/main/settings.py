a42be9cd9ef7b84edae1f48e1a2871e463add8dd
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2023-03-15 15:04:42 +0100

    add jquery and base for static files

--- ui/main/settings.py
+++ ui/main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "jquery",
     "runs",
 ]
 
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
