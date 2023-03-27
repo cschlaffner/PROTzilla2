1df7c4aeac89236e06d8917aaa81cd7445e56999
Lilly Zintl <Lilly.Zintl@student.hpi.de>
2023-03-27 21:08:22 +0200

    Add test for metadataframe orientation.

--- protzilla/importing/metadata_import.py
+++ protzilla/importing/metadata_import.py
@@ -9,8 +9,13 @@ def metadata_import_method(df, file, feature_orientation):
         na_values=[""],
         keep_default_na=True,
     )
+    print(meta_df)
     # always return metadata in the same orientation (features as columns)
     if feature_orientation.startswith("Rows"):
         meta_df = meta_df.transpose()
+        meta_df.reset_index(inplace=True)
+        meta_df.rename(columns=meta_df.iloc[0], inplace=True)
+        meta_df.drop(index=0, inplace=True)
+        meta_df.index = meta_df.index - 1
 
     return df, {"metadata": meta_df}
--- tests/protzilla/importing/test_metadata_import.py
+++ tests/protzilla/importing/test_metadata_import.py
@@ -11,8 +11,29 @@ def test_metadata_import():
     run = Run.create(name)
     run.calculate_and_next(
         metadata_import.metadata_import_method,
-        file=str(PROJECT_PATH / "tests/metadata_cut.csv"),
+        file=str(PROJECT_PATH / "tests/metadata_cut_columns.csv"),
         feature_orientation="Columns (samples in rows, features in columns)",
     )
-    test_metadata = pd.read_csv(str(PROJECT_PATH / "tests/metadata_cut.csv"))
+    test_metadata = pd.read_csv(str(PROJECT_PATH / "tests/metadata_cut_columns.csv"))
     assert run.metadata.equals(test_metadata)
+
+
+def test_metadata_orientation():
+    name1 = "test_run" + random_string()
+    name2 = "test_run" + random_string()
+    run1 = Run.create(name1)
+    run2 = Run.create(name2)
+    run1.calculate_and_next(
+        metadata_import.metadata_import_method,
+        file=str(PROJECT_PATH / "tests/metadata_cut_columns.csv"),
+        feature_orientation="Columns (samples in rows, features in columns)",
+    )
+    run2.calculate_and_next(
+        metadata_import.metadata_import_method,
+        file=str(PROJECT_PATH / "tests/metadata_cut_rows.csv"),
+        feature_orientation="Rows (features in rows, samples in columns)",
+    )
+    print(run1.metadata)
+    print(run2.metadata)
+
+    assert run1.metadata.equals(run2.metadata)
