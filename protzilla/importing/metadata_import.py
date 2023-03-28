6365d29a3045ec90c1e05973d12cd1a6731a38af
antonneubauer <antongneubauer@gmail.com>
2023-03-28 19:46:39 +0200

    with debugging incase it breaks again

--- protzilla/importing/metadata_import.py
+++ protzilla/importing/metadata_import.py
@@ -1,25 +1,62 @@
 import pandas as pd
 
+from protzilla.constants.paths import PROJECT_PATH
+from protzilla.utilities.random import random_string
+
 
 def metadata_import_method(df, file, feature_orientation):
-    meta_df = pd.read_csv(
-        file,
-        sep=",",
-        low_memory=False,
-        na_values=[""],
-        keep_default_na=True,
-        skipinitialspace=True,
-    )
+    if file.endswith(".csv"):
+        print("\nfile", file)
+        meta_df = pd.read_csv(
+            file,
+            sep=",",
+            low_memory=False,
+            na_values=[""],
+            keep_default_na=True,
+            skipinitialspace=True,
+        )
+    elif file.endswith(".xlsx"):
+        meta_df = pd.read_excel(file)
+    elif file.endswith(".psv"):
+        meta_df = pd.read_csv(file, sep="|", low_memory=False)
+    elif file.endswith(".tsv"):
+        meta_df = pd.read_csv(file, sep="\t", low_memory=False)
+    else:
+        raise TypeError(
+            "File format not supported. \
+        Supported file formats are csv, xlsx, psv or tsv"
+        )
+
+    print("\n post read, file:", file)
+    print("meta info", meta_df.info())
+
     # always return metadata in the same orientation (features as columns)
     if feature_orientation.startswith("Rows"):
+        print("file in ROWS", file)
+        print("\npre")
+        print("\nmeta rows\n", meta_df)
+        print("meta rows info ^^", meta_df.info())
+
         meta_df = meta_df.transpose()
         meta_df.reset_index(inplace=True)
         meta_df.rename(columns=meta_df.iloc[0], inplace=True)
         meta_df.drop(index=0, inplace=True)
         meta_df.index = meta_df.index - 1
-        for columns in meta_df:
-            try:
-                meta_df["Age"] = meta_df["Age"].astype("int64")
-            except:
-                pass
+
+        print("\n\npost")
+        print("\nmeta rows\n", meta_df)
+        print("meta rows info", meta_df.info(), "\n")
+
+        file_path = f"{PROJECT_PATH}/tests/protzilla/importing/conversion_tmp_{random_string()}.csv"
+        print(file_path)
+        meta_df.to_csv(file_path, index=False)
+        return metadata_import_method(df, file_path, "Columns")
+
+    elif file.startswith(f"{PROJECT_PATH}/tests/protzilla/importing/conversion_tmp_"):
+        # os.remove(file)
+        print("skipped ROWS for file ", file)
+    else:
+        print("skipped ROWS for file ", file)
+
+    print("\n\n end of import\n\n")
     return df, {"metadata": meta_df}
--- tests/protzilla/importing/test_metadata_import.py
+++ tests/protzilla/importing/test_metadata_import.py
@@ -15,7 +15,10 @@ def test_metadata_import():
         feature_orientation="Columns (samples in rows, features in columns)",
     )
     test_metadata = pd.read_csv(str(PROJECT_PATH / "tests/metadata_cut_columns.csv"))
+    print("test", test_metadata)
+    print("run", run.metadata)
     assert run.metadata.equals(test_metadata)
+    pd.testing.assert_frame_equal(test_metadata, run.metadata)
 
 
 def test_metadata_orientation():
@@ -32,8 +35,19 @@ def test_metadata_orientation():
         metadata_import.metadata_import_method,
         file=str(PROJECT_PATH / "tests/metadata_cut_rows.csv"),
         feature_orientation="Rows (features in rows, samples in columns)",
+        # file=str(PROJECT_PATH / "tests/protzilla/importing/conversion_tmp_fxaCsBjMOAOASmmM.csv"),
+        # feature_orientation="Columns (features in rows, samples in columns)",
     )
     print(run1.metadata.info())
     print(run2.metadata.info())
 
+    print("\n\ncompare")
+    print(run1.metadata.compare(run2.metadata))
+    print("\n\n")
+
+    print(
+        pd.testing.assert_frame_equal(
+            run1.metadata, run2.metadata, check_dtype=False, check_exact=False
+        )
+    )
     assert run1.metadata.equals(run2.metadata)
