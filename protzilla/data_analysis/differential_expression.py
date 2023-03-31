0e9970c31e3f7079be59edc6036423e46e2eb686
brokkoli71 <brokkoli71b@gmail.com>
2023-03-31 12:50:19 +0200

    Import differential_expression methods to remove redundant function definition

--- protzilla/data_analysis/differential_expression.py
+++ protzilla/data_analysis/differential_expression.py
@@ -1,16 +1,11 @@
-from . import differential_expression_t_test
-
 # Import differential_expression_anova's methods to remove redundant function definition
 from .differential_expression_anova import anova, anova_heatmap
+from .differential_expression_t_test import t_test, t_test_volcano_plot
+
 
 # call methods for precommit hook not to delete imports
-anova()
-anova_heatmap()
-
-
-def t_test(*args, **kwargs):
-    return differential_expression_t_test.t_test(*args, **kwargs)
-
-
-def t_test_volcano_plot(*args, **kwargs):
-    return differential_expression_t_test.t_test_volcano_plot(*args, **kwargs)
+def unused():
+    anova(**{})
+    anova_heatmap(**{})
+    t_test(**{})
+    t_test_volcano_plot(**{})
