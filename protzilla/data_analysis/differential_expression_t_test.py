32c2cea7486b47d957635815b8675ce776efe855
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2023-04-03 16:38:10 +0200

    handle t test errors

--- protzilla/data_analysis/differential_expression_t_test.py
+++ protzilla/data_analysis/differential_expression_t_test.py
@@ -1,7 +1,9 @@
-import pandas as pd
-import numpy as np
-from scipy import stats
 import dash_bio as dashbio
+import numpy as np
+import pandas as pd
+from django.contrib import messages
+from scipy import stats
+
 from protzilla.constants.colors import PROTZILLA_DISCRETE_COLOR_SEQUENCE
 from protzilla.data_analysis.differential_expression_helper import (
     apply_multiple_testing_correction,
@@ -31,6 +33,7 @@ def t_test(
     )
     p_values = []
     fold_change = []
+    filtered_proteins = []
 
     for protein in proteins:
         protein_df = intensity_df.loc[intensity_df["Protein ID"] == protein]
@@ -46,32 +49,26 @@ def t_test(
         # if a protein is not present in the sample something should be filtered out
         group1_is_nan = np.isnan(group1_intensities)
         group2_is_nan = np.isnan(group2_intensities)
-        if group1_is_nan.any():
-            raise ValueError(
-                f"Protein {protein} has {group1_is_nan.sum()} missing values \
-                    in group {group1}. Consider revising your \
-                        preprocessing regarding missing values."
-            )
-
-        elif group2_is_nan.any():
-            raise ValueError(
-                f"Protein {protein} has {group2_is_nan.sum()} missing values \
-                    in group {group2}. Consider revising your \
-                        preprocessing regarding missing values."
+        if group1_is_nan.any() or group2_is_nan.any():
+            msg = "There are Proteins with NaN values present in your data. \
+                Please filter them out before running the differential expression analysis."
+            return intensity_df, dict(
+                corrected_p_values=None,
+                log2_fold_change=None,
+                fc_threshold=None,
+                alpha=alpha,
+                corrected_alpha=None,
+                messages=[dict(level=messages.ERROR, msg=msg)],
             )
 
         p = stats.ttest_ind(group1_intensities, group2_intensities)[1]
-        p_values.append(p)
 
-        # TODO: add new error handling
-        # if the intensity of a group for a sample is 0, it should be filtered out
+        # if the intensity of a group for a protein is 0, it should be filtered out
         if np.mean(group1_intensities) == 0 or np.mean(group2_intensities) == 0:
-            raise ValueError(
-                "One of the groups has a mean of 0. Consider filtering \
-                    your data before running the differential expression \
-                        analysis."
-            )
+            filtered_proteins.append(protein)
+            continue
 
+        p_values.append(p)
         fold_change.append(np.mean(group2_intensities) / np.mean(group1_intensities))
 
     (corrected_p_values, corrected_alpha) = apply_multiple_testing_correction(
@@ -84,12 +81,31 @@ def t_test(
     p_values_thresh = alpha if corrected_alpha is None else corrected_alpha
     p_values_mask = corrected_p_values < p_values_thresh
     fold_change_mask = np.abs(log2_fold_change) > fc_threshold
+
+    remaining_proteins = [
+        protein for protein in proteins if protein not in filtered_proteins
+    ]
     de_proteins = [
         protein
-        for i, protein in enumerate(proteins)
+        for i, protein in enumerate(remaining_proteins)
         if p_values_mask[i] and fold_change_mask[i]
     ]
     de_proteins_df = intensity_df.loc[intensity_df["Protein ID"].isin(de_proteins)]
+
+    if len(filtered_proteins) > 0:
+        msg = f"Some proteins were filtered out because they had a mean intensity of 0 in one of the groups."
+        return (
+            de_proteins_df,
+            dict(
+                corrected_p_values=corrected_p_values,
+                log2_fold_change=log2_fold_change,
+                fc_threshold=fc_threshold,
+                alpha=alpha,
+                corrected_alpha=corrected_alpha,
+                filtered_proteins=filtered_proteins,
+                messages=[dict(level=messages.WARNING, msg=msg)],
+            ),
+        )
 
     return (
         de_proteins_df,
--- tests/protzilla/data_analysis/test_differential_expression.py
+++ tests/protzilla/data_analysis/test_differential_expression.py
@@ -1,11 +1,11 @@
+import numpy as np
 import pandas as pd
-import pytest
 
 from protzilla.data_analysis.differential_expression import (
-    t_test,
-    t_test_volcano_plot,
     anova,
     anova_heatmap,
+    t_test,
+    t_test_volcano_plot,
 )
 
 
@@ -73,26 +73,29 @@ def test_differential_expression_t_test(show_figures):
         fig.show()
 
     corrected_p_values = [0.0108, 0.4318, 1.000]
-    log2_fc = [-1, -0.0996, 0]
-    de_proteins = ["Protein1", "Protein2"]
+    log2_fc = [-1, -0.0995, 0]
+    de_proteins = ["Protein1"]
+
+    p_values_rounded = [round(x, 4) for x in current_out["corrected_p_values"]]
+    log2fc_rounded = [round(x, 4) for x in current_out["log2_fold_change"]]
 
     assert (
-        current_out["corrected_p_values"].round(4) == corrected_p_values
+        p_values_rounded == corrected_p_values
     ), f"P \
         values do not match! P Values should be {corrected_p_values}, \
-                but are {current_out['corrected_p_values'].round(4)}"
+                but are {p_values_rounded}"
 
     assert (
-        current_out["log2_fold_change"].round(4) == log2_fc
+        log2fc_rounded == log2_fc
     ), f"log2 \
         fold change does not match! Should be {log2_fc}, \
-                but are {current_out['log2_fold_change'].round(4)}"
+                but are {log2fc_rounded}"
 
     assert (
-        de_proteins_df["Protein ID"].values() == de_proteins
+        de_proteins_df["Protein ID"].unique() == de_proteins
     ), f"\
         Differentially expressed proteins do not match! Should be \
-            {de_proteins} but are {de_proteins_df['Protein ID'].values()}"
+            {de_proteins} but are {de_proteins_df['Protein ID'].unique()}"
 
     assert (
         current_out["fc_threshold"] == test_fc_threshold
@@ -103,6 +106,181 @@ def test_differential_expression_t_test(show_figures):
         current_out["corrected_alpha"] is None
     ), f"corrected alpha does \
         not match"
+
+
+def test_differential_expression_t_test_with_nan():
+    test_intensity_list = (
+        ["Sample1", "Protein1", "Gene1", 18],
+        ["Sample1", "Protein2", "Gene1", 16],
+        ["Sample1", "Protein3", "Gene1", 1],
+        ["Sample2", "Protein1", "Gene1", np.nan],
+        ["Sample2", "Protein2", "Gene1", 15],
+        ["Sample2", "Protein3", "Gene1", 2],
+        ["Sample3", "Protein1", "Gene1", 22],
+        ["Sample3", "Protein2", "Gene1", 14],
+        ["Sample3", "Protein3", "Gene1", 3],
+        ["Sample4", "Protein1", "Gene1", np.nan],
+        ["Sample4", "Protein2", "Gene1", np.nan],
+        ["Sample4", "Protein3", "Gene1", 1],
+        ["Sample5", "Protein1", "Gene1", 10],
+        ["Sample5", "Protein2", "Gene1", 14],
+        ["Sample5", "Protein3", "Gene1", 2],
+        ["Sample6", "Protein1", "Gene1", 12],
+        ["Sample6", "Protein2", "Gene1", np.nan],
+        ["Sample6", "Protein3", "Gene1", 3],
+        ["Sample7", "Protein1", "Gene1", 12],
+        ["Sample7", "Protein2", "Gene1", 13],
+        ["Sample7", "Protein3", "Gene1", 3],
+    )
+
+    test_intensity_df = pd.DataFrame(
+        data=test_intensity_list,
+        columns=["Sample", "Protein ID", "Gene", "Intensity"],
+    )
+
+    test_metadata_list = (
+        ["Sample1", "Group1"],
+        ["Sample2", "Group1"],
+        ["Sample3", "Group1"],
+        ["Sample4", "Group2"],
+        ["Sample5", "Group2"],
+        ["Sample6", "Group2"],
+        ["Sample7", "Group3"],
+    )
+
+    test_metadata_df = pd.DataFrame(
+        data=test_metadata_list,
+        columns=["Sample", "Group"],
+    )
+
+    test_alpha = 0.05
+    test_fc_threshold = 0
+
+    _, current_out = t_test(
+        test_intensity_df,
+        test_metadata_df,
+        grouping="Group",
+        group1="Group1",
+        group2="Group2",
+        multiple_testing_correction_method="Benjamini-Hochberg",
+        alpha=test_alpha,
+        fc_threshold=test_fc_threshold,
+    )
+
+    test_keywords = "NaN values"
+
+    assert "messages" in current_out
+    assert test_keywords in current_out["messages"][0]["msg"]
+
+
+def test_differential_expression_t_test_with_zero_mean(show_figures):
+    test_intensity_list = (
+        ["Sample1", "Protein1", "Gene1", 18],
+        ["Sample1", "Protein2", "Gene1", 16],
+        ["Sample1", "Protein3", "Gene1", 1],
+        ["Sample1", "Protein4", "Gene1", 0],
+        ["Sample2", "Protein1", "Gene1", 20],
+        ["Sample2", "Protein2", "Gene1", 15],
+        ["Sample2", "Protein3", "Gene1", 2],
+        ["Sample2", "Protein4", "Gene1", 0],
+        ["Sample3", "Protein1", "Gene1", 22],
+        ["Sample3", "Protein2", "Gene1", 14],
+        ["Sample3", "Protein3", "Gene1", 3],
+        ["Sample3", "Protein4", "Gene1", 0],
+        ["Sample4", "Protein1", "Gene1", 8],
+        ["Sample4", "Protein2", "Gene1", -5],
+        ["Sample4", "Protein3", "Gene1", 1],
+        ["Sample4", "Protein4", "Gene1", 0],
+        ["Sample5", "Protein1", "Gene1", 10],
+        ["Sample5", "Protein2", "Gene1", 0],
+        ["Sample5", "Protein3", "Gene1", 2],
+        ["Sample5", "Protein4", "Gene1", 18],
+        ["Sample6", "Protein1", "Gene1", 12],
+        ["Sample6", "Protein2", "Gene1", 5],
+        ["Sample6", "Protein3", "Gene1", 3],
+        ["Sample6", "Protein4", "Gene1", 18],
+        ["Sample7", "Protein1", "Gene1", 12],
+        ["Sample7", "Protein2", "Gene1", 13],
+        ["Sample7", "Protein3", "Gene1", 3],
+    )
+
+    test_intensity_df = pd.DataFrame(
+        data=test_intensity_list,
+        columns=["Sample", "Protein ID", "Gene", "Intensity"],
+    )
+
+    test_metadata_list = (
+        ["Sample1", "Group1"],
+        ["Sample2", "Group1"],
+        ["Sample3", "Group1"],
+        ["Sample4", "Group2"],
+        ["Sample5", "Group2"],
+        ["Sample6", "Group2"],
+        ["Sample7", "Group3"],
+    )
+
+    test_metadata_df = pd.DataFrame(
+        data=test_metadata_list,
+        columns=["Sample", "Group"],
+    )
+
+    test_alpha = 0.05
+    test_fc_threshold = 0
+
+    de_proteins_df, current_out = t_test(
+        test_intensity_df,
+        test_metadata_df,
+        grouping="Group",
+        group1="Group1",
+        group2="Group2",
+        multiple_testing_correction_method="Benjamini-Hochberg",
+        alpha=test_alpha,
+        fc_threshold=test_fc_threshold,
+    )
+
+    # fig = t_test_volcano_plot(test_intensity_df, de_proteins_df, current_out, [])[0]
+    # if show_figures:
+    #     fig.show()
+
+    print(de_proteins_df)
+    corrected_p_values = [0.0072, 1.000]
+    log2_fc = [-1, 0]
+    de_proteins = ["Protein1"]
+
+    p_values_rounded = [round(x, 4) for x in current_out["corrected_p_values"]]
+    log2fc_rounded = [round(x, 4) for x in current_out["log2_fold_change"]]
+
+    assert (
+        p_values_rounded == corrected_p_values
+    ), f"P \
+        values do not match! P Values should be {corrected_p_values}, \
+                but are {p_values_rounded}"
+
+    assert (
+        log2fc_rounded == log2_fc
+    ), f"log2 \
+        fold change does not match! Should be {log2_fc}, \
+                but are {log2fc_rounded}"
+
+    assert (
+        de_proteins_df["Protein ID"].unique() == de_proteins
+    ), f"\
+        Differentially expressed proteins do not match! Should be \
+            {de_proteins} but are {de_proteins_df['Protein ID'].unique()}"
+
+    assert (
+        current_out["fc_threshold"] == test_fc_threshold
+    ), f"fold change \
+        threshold does not match!"
+    assert current_out["alpha"] == test_alpha, f"alpha does not match!"
+    assert (
+        current_out["corrected_alpha"] is None
+    ), f"corrected alpha does \
+        not match"
+
+    test_keywords = "mean intensity of 0"
+    assert "messages" in current_out
+    assert test_keywords in current_out["messages"][0]["msg"]
 
 
 def test_differential_expression_anova(show_figures):
