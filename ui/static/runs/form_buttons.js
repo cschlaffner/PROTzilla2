e622e1f224832b286a26ac47ce666609103f44f9
antonneubauer <antongneubauer@gmail.com>
2023-04-04 18:16:18 +0200

    almost working buttons

--- ui/static/runs/form_buttons.js
+++ ui/static/runs/form_buttons.js
@@ -11,24 +11,30 @@ disable_plot = function (bool) {
 }
 
 get_clean_form_data = function (form) {
-    let params = new FormData(form);
-    params.delete("csrfmiddlewaretoken")
-    console.log("form data", params)
-    return params
+    let formDataArray = form.serializeArray()
+    let formDataDict = {};
+    for (let i = 0; i < formDataArray.length; i++) {
+        let fieldName = formDataArray[i].name;
+        let fieldValue = formDataArray[i].value;
+        formDataDict[fieldName] = fieldValue;
+    }
+    console.log("bla")
+    console.log("form data", formDataDict)
+    delete formDataDict["csrfmiddlewaretoken"]
+    return formDataDict
 }
 
-set_calc_params = function () {
-    let params = get_clean_form_data(document.querySelector('#calc_form'))
-    sessionStorage.setItem(run_name + "_calc_params", params.serialize())
+set_calc_params = function (form) {
+    let params = get_clean_form_data(form)
+    sessionStorage.setItem(run_name + "_calc_params", JSON.stringify(params))
     console.log("set calc params", params)
-    console.log("set calc params serialized", params.serialize())
     return params
 }
 
-set_plot_params = function () {
-    let params = get_clean_form_data(document.querySelector('#plot_form'))
+set_plot_params = function (form) {
+    let params = get_clean_form_data(form)
     sessionStorage.setItem(run_name + "_plot_params", JSON.stringify(params))
-    console.log("plot params", params)
+    console.log("set plot params", params)
     return params
 }
 
@@ -38,69 +44,94 @@ $(document).ready(function () {
     const next_form = document.querySelector('#next_form');
     const back_form = document.querySelector('#back_form');
 
+    // server gib mir calc_params
+    // gib mir plot_params
+
     if (sessionStorage.getItem(run_name + "_first_load") === "true") {
+        console.log("first load")
         disable_calc(false)
-        disable_plot(show_plot_button) // not sure if this works correctly!
+        disable_plot(true)
         sessionStorage.setItem(run_name + "_first_load", "false")
     } else {
+        console.log("disable plot:", sessionStorage.getItem(run_name + "_disabled_plot"))
         disable_calc(sessionStorage.getItem(run_name + "_disabled_calc"))
         disable_plot(sessionStorage.getItem(run_name + "_disabled_plot"))
     }
 
     calc_form.addEventListener('submit', function () {
-        set_calc_params()
+        set_calc_params($(this))
         disable_plot(false)
         disable_calc(true)
         sessionStorage.setItem(run_name + '_calculated', "true")
         sessionStorage.setItem(run_name + "_plotted", "false")
     })
 
-    calc_form.addEventListener('change', function (){
-        if (sessionStorage.getItem(run_name + "_calculated") === "true"){
-            let current_params = get_clean_form_data(calc_form)
-            let calc_params = JSON.parse(sessionStorage.getItem(run_name + "_calc_params"))
-            console.log("change calc, calc_params", calc_params)
-            for (let [key, value] of calc_params.entries()){
-                if (current_params.get(key) !== value){
+    calc_form.addEventListener('change', function () {
+        if (!JSON.parse(sessionStorage.getItem(run_name + "_calc_params"))) {
+            return
+        }
+        let current_params = get_clean_form_data($(this))
+        let calc_params = JSON.parse(sessionStorage.getItem(run_name + "_calc_params"))
+
+        for (let key in calc_params) {
+            if (calc_params.hasOwnProperty(key)) {
+                let value = calc_params[key]
+                if (current_params[key] !== value) {
                     disable_calc(false)
                     disable_plot(true)
                     sessionStorage.setItem(run_name + "_calculated", "false")
+                    sessionStorage.setItem(run_name + "_plotted", "false")
+                    // console.log("unequal: current vs prev")
                     return
                 }
             }
-            // all equal
-            if (sessionStorage.getItem(run_name + "_plotted")){
-                disable_plot(true)
-            } // TODO: change event listener on plot form, set "_plotted" to false when change
         }
+        sessionStorage.setItem(run_name + "_calculated", "true")
+        disable_calc(true)
+
+        if (sessionStorage.getItem(run_name + "_plotted") === "true") {
+            disable_plot(true)
+        } else {
+            $('#plot_form').trigger("change")
+        }
+
     })
 
-    plot_form.addEventListener('submit', function (){
-        set_plot_params()
+    plot_form.addEventListener('submit', function () {
+        set_plot_params($(this))
         disable_plot(true)
         sessionStorage.setItem(run_name + "_plotted", "true")
     })
 
     plot_form.addEventListener("change", function () {
-        if (sessionStorage.getItem(run_name + "_plotted")){
-            let current_params = get_clean_form_data(plot_form)
-            let plot_params = JSON.parse(sessionStorage.getItem(run_name + "_plot_params"))
-            for (let [key, value] of plot_params.entries()){
-                if (current_params.get(key) !== value){
+        console.log("change plot form")
+
+        // if (!JSON.parse(sessionStorage.getItem(run_name + "_plot_params"))) {
+        //     return
+        // }
+        if (!JSON.parse(sessionStorage.getItem(run_name + "_calculated"))) {
+            disable_plot(true)
+            sessionStorage.setItem(run_name + "_plotted", "false")
+            console.log("json parse")
+            return
+        }
+
+        let current_params = get_clean_form_data($(this))
+        let plot_params = JSON.parse(sessionStorage.getItem(run_name + "_plot_params"))
+
+        for (let key in plot_params) {
+            if (plot_params.hasOwnProperty(key)) {
+                let value = plot_params[key]
+                if (current_params[key] !== value) {
                     disable_plot(false)
                     sessionStorage.setItem(run_name + "_plotted", "false")
                     return
                 }
             }
-            // same plot params as last plot
-            disable_plot(true)
-            sessionStorage.setItem(run_name + "_plotted", "true") // this is redundant -> if-case
         }
-        else if (sessionStorage.getItem(run_name + "_calculated")){
-            disable_plot(false)
-        } else {
-            disable_plot(true)
-        }
+        disable_plot(true)
+        sessionStorage.setItem(run_name + "_plotted", "true") // this is redundant -> if-case ??
+        console.log("end of plot change")
     })
 
     next_form.addEventListener("submit", function () {
