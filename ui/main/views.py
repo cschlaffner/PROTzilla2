f28f892db2d4d29eac2e66fce1a17ea2161b870a
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2023-04-06 20:23:09 +0200

    add logos and basic navbar

--- ui/main/views.py
+++ ui/main/views.py
@@ -1,8 +1,5 @@
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse(
-        "Hello, world. You're at the main index.<br><a href='/runs'>Runs</a>"
-    )
+    return render(request, 'base.html', {})
