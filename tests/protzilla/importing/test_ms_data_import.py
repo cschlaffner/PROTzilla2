3f63541b5e247eac5d80d3cc76fc660d9a9a35ef
Sara Grau <sara.grau-i-blade@student.hpi.uni-potsdam.de>
2023-04-08 18:44:01 +0200

    adopted review suggestions
    - Use of fixtures
    - introduced pytest.mark.parametrize
    - fixed assert

--- tests/protzilla/importing/test_ms_data_import.py
+++ tests/protzilla/importing/test_ms_data_import.py
@@ -1,10 +1,34 @@
+import pytest
+
 from protzilla.constants.paths import PROJECT_PATH
 from protzilla.importing import ms_data_import
 import pandas as pd
 
 
-def test_ms_quant_import():
-    intensity_names = [
+@pytest.fixture
+def ms_fragger_raw_df():
+    return pd.read_csv(
+        f"{PROJECT_PATH}/tests/combined_protein_method_small_cut.tsv",
+        sep="\t",
+        low_memory=False,
+        na_values=["", 0],
+        keep_default_na=True,
+    )
+
+
+def intensity_values_for_sample_and_intensity(
+    ms_fragger_raw_df, sample_name, intensity_name
+):
+    intensity_name_raw_df = sample_name + " " + intensity_name
+    df = ms_fragger_raw_df[["Protein ID", intensity_name_raw_df]]
+    df.sort_values(by=["Protein ID"], ignore_index=True, inplace=True)
+    df = df[intensity_name_raw_df]
+    return df
+
+
+@pytest.mark.parametrize(
+    "intensity_name",
+    [
         "Intensity",
         "MaxLFQ Total Intensity",
         "MaxLFQ Intensity",
@@ -14,28 +38,23 @@ def test_ms_quant_import():
         "Unique Intensity",
         "Spectral Count",
         "Total Spectral Count",
-    ]
-    raw_df = pd.read_csv(
-        f"{PROJECT_PATH}/tests/combined_protein_method_small_cut.tsv",
-        sep="\t",
-        low_memory=False,
-        na_values=["", 0],
-        keep_default_na=True,
+    ],
+)
+def test_ms_fragger_import(ms_fragger_raw_df, intensity_name):
+    intensity_df, _ = ms_data_import.ms_fragger_import(
+        _=None,
+        file_path=f"{PROJECT_PATH}/tests/combined_protein_method_small_cut.tsv",
+        intensity_name=intensity_name,
     )
 
-    for intensity_name in intensity_names:
-        intensity_df, _ = ms_data_import.ms_fragger_import(
-            _=None,
-            file_path=f"{PROJECT_PATH}/tests/combined_protein_method_small_cut.tsv",
-            intensity_name=intensity_name,
+    samples = intensity_df["Sample"].unique().tolist()
+    for sample in samples:
+        left = intensity_values_for_sample_and_intensity(
+            ms_fragger_raw_df, sample, intensity_name
         )
-        samples = intensity_df["Sample"].unique().tolist()
-        for sample in samples:
-            intensity_name_raw_df = sample + " " + intensity_name
-            left = raw_df[["Protein ID", intensity_name_raw_df]]
-            left.sort_values(by=["Protein ID"], ignore_index=True, inplace=True)
-            left = left[[intensity_name_raw_df]]
 
-            right = intensity_df.loc[intensity_df["Sample"] == sample, [intensity_name]]
+        right = intensity_df.loc[intensity_df["Sample"] == sample, intensity_name]
 
-            left.equals(right)
+        pd.testing.assert_series_equal(
+            left, right, check_index=False, check_names=False, check_dtype=False
+        )
