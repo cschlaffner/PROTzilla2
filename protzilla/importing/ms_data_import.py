b53f7793362d7286f01fb68022f131594800ce9a
Sara Grau <sara.grau-i-blade@student.hpi.uni-potsdam.de>
2023-04-08 10:27:44 +0200

    fixed matching issues with intensity names

--- protzilla/importing/ms_data_import.py
+++ protzilla/importing/ms_data_import.py
@@ -31,22 +31,36 @@ def max_quant_import(_, file_path, intensity_name):
 
 
 def ms_fragger_import(_, file_path, intensity_name):
-    # iBAQ doesn't exist inmas fragger
-    assert intensity_name in ["Intensity", "iBAQ", "LFQ intensity"]
+    assert intensity_name in [
+        "Intensity",
+        "MaxLFQ Total Intensity",
+        "MaxLFQ Intensity",
+        "Total Intensity",
+        "MaxLFQ Unique Intensity",
+        "Unique Spectral Count",
+        "Unique Intensity",
+        "Spectral Count",
+        "Total Spectral Count",
+    ]
     assert Path(file_path).is_file()
     selected_columns = ["Protein ID", "Gene"]
-    df = pd.read_csv(
+    read = pd.read_csv(
         file_path,
         sep="\t",
         low_memory=False,
         na_values=["", 0],
         keep_default_na=True,
     )
-    id_df = df[selected_columns]
-    intensity_df = df.filter(regex=f"{intensity_name}$", axis=1)
-    # TODO: which Intensity in the dataframe matches the intensity name "Intensity"?
-    #  and what is the differnce between total, unique... intensity?
-    intensity_df.columns = [c[: -len(intensity_name) + 1] for c in intensity_df.columns]
+    id_df = read[selected_columns]
+    intensity_df = read.filter(regex=f"{intensity_name}$", axis=1)
+    intensity_df.columns = [
+        c[: -(len(intensity_name) + 1)] for c in intensity_df.columns
+    ]
+    intensity_df = intensity_df.drop(
+        columns=intensity_df.filter(
+            regex="MaxLFQ Total$|MaxLFQ$|Total$|MaxLFQ Unique$|Unique$", axis=1
+        ).columns
+    )
     molten = pd.melt(
         pd.concat([id_df, intensity_df], axis=1),
         id_vars=selected_columns,
