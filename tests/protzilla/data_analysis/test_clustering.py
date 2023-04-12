01446b2909794aedf79c6f37094c853cd3f37a57
Sara Grau <sara.grau-i-blade@student.hpi.uni-potsdam.de>
2023-04-12 20:41:34 +0200

    started drafting test

---
+++ tests/protzilla/data_analysis/test_clustering.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from protzilla.data_analysis.clustering import k_means
+import pytest
+import numpy as np
+
+
+@pytest.fixture
+def clustering_df():
+    clustering_list = (
+        ["Sample1", "Protein1", "Gene1", 18],
+        ["Sample1", "Protein2", "Gene1", 16],
+        ["Sample1", "Protein3", "Gene1", 1],
+        ["Sample2", "Protein1", "Gene1", 20],
+        ["Sample2", "Protein2", "Gene1", 18],
+        ["Sample2", "Protein3", "Gene1", 2],
+        ["Sample3", "Protein1", "Gene1", 22],
+        ["Sample3", "Protein2", "Gene1", 19],
+        ["Sample3", "Protein3", "Gene1", 3],
+        ["Sample4", "Protein1", "Gene1", 8],
+        ["Sample4", "Protein2", "Gene1", 15],
+        ["Sample4", "Protein3", "Gene1", 1],
+        ["Sample5", "Protein1", "Gene1", 10],
+        ["Sample5", "Protein2", "Gene1", 14],
+        ["Sample5", "Protein3", "Gene1", 2],
+        ["Sample6", "Protein1", "Gene1", 12],
+        ["Sample6", "Protein2", "Gene1", 13],
+        ["Sample6", "Protein3", "Gene1", 3],
+        ["Sample7", "Protein1", "Gene1", 12],
+        ["Sample7", "Protein2", "Gene1", 13],
+        ["Sample7", "Protein3", "Gene1", 3],
+    )
+
+    clustering_df = pd.DataFrame(
+        data=clustering_list,
+        columns=["Sample", "Protein ID", "Gene", "Intensity"],
+    )
+
+    return clustering_df
+
+
+def test_k_means(clustering_df):
+    _, d = k_means(clustering_df, n_clusters=8)
+
+
+# implement nan handling test for k means
+
+# implement n_clusters error test for k means
