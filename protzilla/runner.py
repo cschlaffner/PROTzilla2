294b60eb18ed19dc9c2c1254ca51510bdf3ee434
antonneubauer <antongneubauer@gmail.com>
2023-04-13 17:24:48 +0200

    functioning

--- protzilla/runner.py
+++ protzilla/runner.py
@@ -1,10 +1,11 @@
 import logging
+import os
 import sys
+from pathlib import Path
 
+from .constants.paths import RUNS_PATH
 from .run import Run
 from .utilities.random import random_string
-
-# from .constants.paths import PROJECT_PATH
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
@@ -17,18 +18,19 @@ class Runner:
             args.name if args.name is not None else f"runner_{random_string()}"
         )
         self.df_mode = args.dfMode if args.dfMode is not None else "disk"
+
+        if os.path.exists(RUNS_PATH / self.run_name):
+            self._overwrite_run_prompt()
+
         self.run = Run.create(
             run_name=self.run_name,
             workflow_config_name=self.args.workflow,
             df_mode=self.df_mode,
         )
 
-        # print(self.run.workflow_config.items())
-        # print()
-        # for section, steps in self.run.workflow_config["sections"].items():
-        #     for step in steps["steps"]:
-        #         for param in step["parameters"]:
-        #             print(section, step, param)
+        if self.args.allPlots:
+            self.plots_path = Path(f"{self.run.run_path}/plots")
+            self.plots_path.mkdir()
 
         self.compute_workflow()
 
@@ -36,47 +38,28 @@ class Runner:
         print("\n\n------ compute workflow\n")
         for section, steps in self.run.workflow_config["sections"].items():
             for step in steps["steps"]:
-                logging.info(
-                    f"performing step: {section, step['name'], step['method']}"
-                )
                 if section == "importing":
-                    self._importing(section, step)
-                    self.run.next_step(f"{self.run.current_workflow_location()}")
+                    self._importing(step)
                 elif section == "data_analysis":
                     logging.warning("data_analysis not implemented yet")
                     exit(0)
                 else:
-                    logging.log(
-                        0, f"performing step: {section, step['name'], step['method']}"
+                    logging.info(
+                        f"performing step: {*self.run.current_workflow_location(),}"
                     )
                     self.run.perform_calculation_from_location(
-                        section, step["name"], step["method"], step["parameters"]
+                        *self.run.current_workflow_location(), step["parameters"]
                     )
-
                     if self.args.allPlots:
-                        params = dict()
-                        if "graphs" in step:
-                            print(step["graphs"])
-                            params = self._serialize_graphs(step["graphs"])
+                        self._create_plots_for_step(section, step)
+                self.run.next_step(f"{self.run.current_workflow_location()}")
 
-                        print(params)
-                        if "transformation" in step["name"]:
-                            pass
-                        self.run.create_plot_from_location(
-                            *self.run.current_workflow_location(),
-                            parameters=params,
-                        )
-                        for plot in self.run.plots:
-                            plot.show()
-                    self.run.next_step(f"{self.run.current_workflow_location()}")
-                    # assert False
-
-    def _importing(self, section, step):
+    def _importing(self, step):
         if step["name"] == "ms_data_import":
             params = step["parameters"]
             params["file_path"] = self.args.msDataPath
-            self._perform_step(section, step, params)
-            logging.log(0, "imported MS Data")
+            self._perform_current_step(params)
+            logging.info(0, "imported MS Data")
 
         elif step["name"] == "metadata_import":
             if self.args.metaDataPath is None:
@@ -86,16 +69,43 @@ class Runner:
                 )
             params = step["parameters"]
             params["file_path"] = self.args.metaDataPath
-            self._perform_step(section, step, params)
-            logging.log(0, "imported Meta Data")
+            self._perform_current_step(params)
+            logging.info(0, "imported Meta Data")
 
         else:
             raise ValueError(f"Cannot find step with name {step['name']} in importing")
 
-    def _perform_step(self, section, step, params):
+    def _perform_current_step(self, params):
         self.run.perform_calculation_from_location(
-            section, step["name"], step["method"], params
+            *self.run.current_workflow_location(), params
         )
+
+    def _create_plots_for_step(self, section, step):
+        params = dict()
+        if "graphs" in step:
+            params = self._serialize_graphs(step["graphs"])
+
+        self.run.create_plot_from_location(
+            *self.run.current_workflow_location(),
+            parameters=params,
+        )
+        for plot in self.run.plots:
+            plot_path = f"{self.plots_path}/{self.run.step_index}-{section}-{step['name']}-{step['method']}.html"
+            plot.write_html(plot_path)
+
+    def _overwrite_run_prompt(self):
+        answer = input(
+            "a run with the this name already exists. "
+            "Do you want to overwrite it? [y/n]: "
+        )
+        if answer in ["n", "no"]:
+            print("exiting")
+            exit(0)
+        elif answer in ["y", "yes"]:
+            return
+        else:
+            print("\n## Please answer with one of the given options ##")
+            self._overwrite_run_prompt()
 
     def _serialize_graphs(self, graphs):
         return {k: v for graph in graphs for k, v in graph.items()}
