91bbde6fbabc8f5c65df9e71a0af9c710ac59776
antonneubauer <antongneubauer@gmail.com>
2023-04-13 12:15:40 +0200

    basic functial runner

--- runner-cli.py
+++ runner-cli.py
@@ -3,16 +3,28 @@ import argparse
 from protzilla.runner import Runner
 
 parser = argparse.ArgumentParser(
-    argument_default=argparse.SUPPRESS,
+    argument_default=None,
 )
 
-parser.add_argument("workflow", action="store")
-parser.add_argument("ms-data-path", action="store")
-parser.add_argument("--meta-data-path", action="store")
-parser.add_argument("-n", "--name", action="store")
-parser.add_argument("--df-mode", action="store")
+# paths to files have to be supplied in quotes on the command line
+parser.add_argument(
+    "workflow", action="store", help="name of a workflow, saved in /user_data/workflows"
+)
+parser.add_argument(
+    "msDataPath", action="store", help="path to the dataset you want to compute"
+)
+parser.add_argument(
+    "--metaDataPath", action="store", help="path to the metadata for your dataset"
+)
+parser.add_argument("-n", "--name", action="store", help="Name of the run")
+parser.add_argument("--dfMode", action="store", help="Disk or memory")
+parser.add_argument(
+    "-p",
+    "--allPlots",
+    action="store_true",
+    help="create all plots and export them, default: false",
+)
 
 args = parser.parse_args()
-print("args:", args)
 
 runner = Runner(args)
