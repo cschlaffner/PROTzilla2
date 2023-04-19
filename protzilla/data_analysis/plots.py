e8c996a8344756d68b23fc517c50e534c018af97
Sara Grau <sara.grau-i-blade@student.hpi.uni-potsdam.de>
2023-04-19 18:52:14 +0200

    added more tests for scatter plot

--- protzilla/data_analysis/plots.py
+++ protzilla/data_analysis/plots.py
@@ -1,65 +1,68 @@
-from protzilla.utilities.transform_dfs import long_to_wide, is_long_format
-import plotly.express as px
 import pandas as pd
+import plotly.express as px
+from django.contrib import messages
+
+from protzilla.utilities.transform_dfs import is_long_format, long_to_wide
 
 
 # Test if input handling works in frontend
-def scatter_plot_2d(
-    input_df,
-    color_df,
+# handle color_df parameter optional!!
+def scatter_plot(
+    intensity_df: pd.DataFrame,
+    input_df: pd.DataFrame,
+    color_df: pd.DataFrame,
 ):
+    """
+    scatterplot blablabla
+    :param input_df: the dataframe that should be plotted. It should have either 2 \
+    or 3 dimension
+    :type input_df: pd.Dataframe
+    :param color_df: the Dataframe with one column according to which the marks should \
+    be colored
+    :type color_df: pd.Dataframe
+    """
     intensity_df_wide = long_to_wide(input_df) if is_long_format(input_df) else input_df
     try:
-        assert intensity_df_wide.shape[1] == 2
-
-        intensity_df_wide = pd.concat([intensity_df_wide, color_df], axis=1)
-        x_name = intensity_df_wide.columns[0]
-        y_name = intensity_df_wide.columns[1]
-        color_name = color_df.columns[0]
-        fig = px.scatter(intensity_df_wide, x=x_name, y=y_name, color=color_name)
+        if color_df.shape[1] == 1:
+            if intensity_df_wide.shape[1] == 2:
+                intensity_df_wide = pd.concat([intensity_df_wide, color_df], axis=1)
+                x_name = intensity_df_wide.columns[0]
+                y_name = intensity_df_wide.columns[1]
+                color_name = color_df.columns[0]
+                fig = px.scatter(
+                    intensity_df_wide, x=x_name, y=y_name, color=color_name
+                )
+            elif intensity_df_wide.shape[1] == 3:
+                intensity_df_wide = pd.concat([intensity_df_wide, color_df], axis=1)
+                x_name = intensity_df_wide.columns[0]
+                y_name = intensity_df_wide.columns[1]
+                z_name = intensity_df_wide.columns[2]
+                color_name = color_df.columns[0]
+                fig = px.scatter_3d(
+                    intensity_df_wide, x=x_name, y=y_name, z=z_name, color=color_name
+                )
+            else:
+                raise Exception(
+                    "The dimensions of the DataFrame are either too high or too low."
+                )
+        else:
+            raise Exception("The color dataframe should have 1 dimension only")
         return [fig]
-    except AssertionError as e:
+    except Exception as e:
         msg = ""
         if intensity_df_wide.shape[1] < 2:
             msg = (
                 f"The input dataframe has {intensity_df_wide.shape[1]} feature. "
                 f"Consider using another plot to visualize your data"
             )
-        elif intensity_df_wide.shape[1] > 2:
-            msg = (
-                f"The input dataframe has {intensity_df_wide.shape[1]} features. "
-                f"Consider reducing the dimensionality of your data"
-            )
-        return [{}, msg]
-
-
-def scatter_plot_3d(
-    input_df,
-    color_df,
-):
-    intensity_df_wide = long_to_wide(input_df) if is_long_format(input_df) else input_df
-    try:
-        assert intensity_df_wide.shape[1] == 3
-
-        intensity_df_wide = pd.concat([intensity_df_wide, color_df], axis=1)
-        x_name = intensity_df_wide.columns[0]
-        y_name = intensity_df_wide.columns[1]
-        z_name = intensity_df_wide.columns[2]
-        color_name = color_df.columns[0]
-        fig = px.scatter_3d(
-            intensity_df_wide, x=x_name, y=y_name, z=z_name, color=color_name
-        )
-        return [fig]
-    except AssertionError as e:
-        msg = ""
-        if intensity_df_wide.shape[1] < 3:
-            msg = (
-                f"The input dataframe has {intensity_df_wide.shape[1]} features. "
-                f"Consider using a 2d scatter plot to visualize your data"
-            )
         elif intensity_df_wide.shape[1] > 3:
             msg = (
                 f"The input dataframe has {intensity_df_wide.shape[1]} features. "
                 f"Consider reducing the dimensionality of your data"
             )
-        return [{}, msg]
+        elif color_df.shape[1] != 1:
+            msg = "The color dataframe should have 1 dimension only"
+        return [
+            dict(),
+            dict(messages=[dict(level=messages.ERROR, msg=msg, trace=str(e))]),
+        ]
--- tests/protzilla/data_analysis/test_plots.py
+++ tests/protzilla/data_analysis/test_plots.py
@@ -1,10 +1,11 @@
+import pytest
+
 from protzilla.data_analysis.plots import *
 from tests.protzilla.data_analysis.test_clustering import *
-import pytest
 
 
 @pytest.fixture
-def wide_df():
+def wide_2d_df():
     return pd.DataFrame(
         np.array(
             [
@@ -15,6 +16,38 @@ def wide_df():
             ]
         ),
         columns=["Protein1", "Protein2"],
+        index=["Sample1", "Sample2", "Sample3", "Sample4"],
+    )
+
+
+@pytest.fixture
+def wide_3d_df():
+    return pd.DataFrame(
+        np.array(
+            [
+                [4, 10, 3],
+                [8, 2, 4],
+                [2, 7, 1],
+                [13, 5, 7],
+            ]
+        ),
+        columns=["Protein1", "Protein2", "Protein3"],
+        index=["Sample1", "Sample2", "Sample3", "Sample4"],
+    )
+
+
+@pytest.fixture
+def wide_4d_df():
+    return pd.DataFrame(
+        np.array(
+            [
+                [4, 10, 3, 2],
+                [8, 2, 4, 7],
+                [2, 7, 1, 4],
+                [13, 5, 7, 1],
+            ]
+        ),
+        columns=["Protein1", "Protein2", "Protein3", "Protein4"],
         index=["Sample1", "Sample2", "Sample3", "Sample4"],
     )
 
@@ -35,8 +68,22 @@ def color_df():
     )
 
 
-def test_scatter_plot_2d(wide_df, color_df, show_figures=True):
-    fig = scatter_plot_2d(wide_df, color_df)
+def test_scatter_plot_2d(wide_2d_df, color_df, show_figures=True):
+    fig = scatter_plot(wide_2d_df, wide_2d_df, color_df)[0]
     if show_figures:
         fig.show()
     return
+
+
+def test_scatter_plot_3d(wide_3d_df, color_df, show_figures=True):
+    fig = scatter_plot(wide_3d_df, wide_3d_df, color_df)[0]
+    if show_figures:
+        fig.show()
+    return
+
+
+def test_scatter_plot_4d_df(wide_4d_df, color_df):
+    result = scatter_plot(wide_4d_df, wide_4d_df, color_df)
+
+    assert "messages" in result[-1]
+    assert "Consider reducing the dimensionality" in result[-1]["messages"][0]["msg"]
