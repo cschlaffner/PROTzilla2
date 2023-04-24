205610936dcd005e0119d6337b23ecc2a910562e
antonneubauer <antongneubauer@gmail.com>
2023-04-24 15:55:01 +0200

    restructure tests to hannes liking

--- tests/protzilla/test_runner.py
+++ tests/protzilla/test_runner.py
@@ -24,67 +24,23 @@ def metadata_path():
     return "tests/metadata_cut_columns.csv"
 
 
-@pytest.fixture
-def importing_args(tests_folder_name, metadata_path, ms_data_path):
-    return [
+def test_runner_imports(monkeypatch, tests_folder_name, ms_data_path, metadata_path):
+    importing_args = [
         "only_import",  # expects max-quant import, metadata import
         ms_data_path,
         f"--run_name={tests_folder_name}/test_runner_{random_string()}",
         f"--meta_data_path={metadata_path}",
     ]
 
-
-@pytest.fixture
-def no_metadata_args(tests_folder_name, ms_data_path):
-    return [
-        "only_import",  # expects max-quant import, metadata import
-        ms_data_path,
-        f"--run_name={tests_folder_name}/test_runner_{random_string()}",
-    ]
-
-
-@pytest.fixture
-def calculating_args(tests_folder_name, ms_data_path):
-    return [
-        # expects: max-quant, filter proteins
-        "only_import_and_filter_proteins",
-        ms_data_path,
-        f"--run_name={tests_folder_name}/test_runner_{random_string()}",
-    ]
-
-
-@pytest.fixture
-def plot_args(tests_folder_name, metadata_path, ms_data_path):
-    return [
-        # expects: max-quant, filter proteins
-        "only_import_and_filter_proteins",
-        ms_data_path,
-        f"--run_name={tests_folder_name}/test_runner_{random_string()}",
-        "--all_plots",
-    ]
-
-
-def prep_run_and_compute(monkeypatch, args):
-    kwargs = args_parser().parse_args(args).__dict__
+    kwargs = args_parser().parse_args(importing_args).__dict__
     runner = Runner(**kwargs)
 
     mock_perform = mock.MagicMock()
     mock_add_step = mock.MagicMock()
-    mock_plot = mock.MagicMock()
-
     monkeypatch.setattr(runner, "_perform_current_step", mock_perform)
     monkeypatch.setattr(runner.run.history, "add_step", mock_add_step)
-    monkeypatch.setattr(runner.run, "create_plot_from_location", mock_plot)
 
     runner.compute_workflow()
-    return dict(
-        runner=runner, perform=mock_perform, add_step=mock_add_step, plot=mock_plot
-    )
-
-
-def test_runner_imports(monkeypatch, importing_args, ms_data_path, metadata_path):
-    out = prep_run_and_compute(monkeypatch, importing_args)
-    mock_perform = out["perform"]
 
     calls = [
         mock.call({"intensity_name": "iBAQ", "file_path": ms_data_path}),
@@ -100,14 +56,43 @@ def test_runner_imports(monkeypatch, importing_args, ms_data_path, metadata_path
     assert mock_perform.call_count == 2
 
 
-def test_runner_raises_error_for_missing_metadata_arg(monkeypatch, no_metadata_args):
-    pytest.raises(ValueError, prep_run_and_compute, monkeypatch, no_metadata_args)
+def test_runner_raises_error_for_missing_metadata_arg(
+    monkeypatch, tests_folder_name, ms_data_path
+):
+    no_metadata_args = [
+        "only_import",  # expects max-quant import, metadata import
+        ms_data_path,
+        f"--run_name={tests_folder_name}/test_runner_{random_string()}",
+    ]
+    with pytest.raises(ValueError) as e:
+        kwargs = args_parser().parse_args(no_metadata_args).__dict__
+        runner = Runner(**kwargs)
+
+        monkeypatch.setattr(runner, "_perform_current_step", mock.MagicMock())
+        monkeypatch.setattr(runner.run.history, "add_step", mock.MagicMock())
+        monkeypatch.setattr(runner.run, "create_plot_from_location", mock.MagicMock())
+
+        runner.compute_workflow()
 
 
-def test_runner_calculates(monkeypatch, calculating_args, ms_data_path, metadata_path):
-    out = prep_run_and_compute(monkeypatch, calculating_args)
-    mock_perform = out["perform"]
-    mock_plot = out["plot"]
+def test_runner_calculates(monkeypatch, tests_folder_name, ms_data_path, metadata_path):
+    calculating_args = [
+        # expects: max-quant, filter proteins
+        "only_import_and_filter_proteins",
+        ms_data_path,
+        f"--run_name={tests_folder_name}/test_runner_{random_string()}",
+    ]
+    kwargs = args_parser().parse_args(calculating_args).__dict__
+    runner = Runner(**kwargs)
+
+    mock_perform = mock.MagicMock()
+    mock_plot = mock.MagicMock()
+
+    monkeypatch.setattr(runner, "_perform_current_step", mock_perform)
+    monkeypatch.setattr(runner.run.history, "add_step", mock.MagicMock())
+    monkeypatch.setattr(runner.run, "create_plot_from_location", mock_plot)
+
+    runner.compute_workflow()
 
     mock_perform.assert_any_call({"threshold": 0.2})
     # import, filter proteins
@@ -115,9 +100,24 @@ def test_runner_calculates(monkeypatch, calculating_args, ms_data_path, metadata
     mock_plot.assert_not_called()
 
 
-def test_runner_plots(monkeypatch, plot_args):
-    out = prep_run_and_compute(monkeypatch, plot_args)
-    mock_plot = out["plot"]
+def test_runner_plots(monkeypatch, tests_folder_name, ms_data_path):
+    plot_args = [
+        # expects: max-quant, filter proteins
+        "only_import_and_filter_proteins",
+        ms_data_path,
+        f"--run_name={tests_folder_name}/test_runner_{random_string()}",
+        "--all_plots",
+    ]
+    kwargs = args_parser().parse_args(plot_args).__dict__
+    runner = Runner(**kwargs)
+
+    mock_plot = mock.MagicMock()
+
+    monkeypatch.setattr(runner, "_perform_current_step", mock.MagicMock())
+    monkeypatch.setattr(runner.run.history, "add_step", mock.MagicMock())
+    monkeypatch.setattr(runner.run, "create_plot_from_location", mock_plot)
+
+    runner.compute_workflow()
 
     mock_plot.assert_called_once_with(
         "data_preprocessing",
