69774f65026f7e977682e8b8a1c459c412b73f3b
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2023-04-25 12:07:22 +0200

    Add styling (#106)
    * file structure
    
    * Fix broken message tags
    
    * add django bootstrap and basic file structure
    
    * extract css
    
    * add logos and basic navbar
    
    * add favicon
    
    * add basic protzilla colors
    
    * darken grey
    
    was barely visible before
    
    * WIP styling
    
    add white button. update side bar
    
    * fix sidebar height
    
    also change grey again
    
    * fix plots, show run name in navbar
    
    needed to extract navbar for this
    
    * only show history heading if history is displayed
    
    * start reworking runs page
    
    * style basic create runs page
    
    * extract css for index run page
    
    * remove newline in fields.py
    
    does not get shown in frontend, so it can be removed here
    
    * Move bootstrap messages to a better location
    
    * WIP runs page
    
    * only show line when history is displayed
    
    * change button order
    
    * WIP styling main run page
    
    - move graph and other params next to each other if there are other params
    - add more padding for content section
    
    * make run-name more centered in navbar
    
    * add margins to forms
    
    * put calculate and plot button next to each other
    
    when there are no plot parameters
    
    * remove new lines in history
    
    * display name instead of location
    
    * Improve History Layout
    
    * remove df head from displayed history
    
    * add section headings for history
    
    * add comments to make html more readable
    
    * move name field next to step heading
    
    - removed label from field_text, might change to if in case this is needed somewhere else
    
    * update buttons
    
    - move back and next button to the right
    - change button colors
    - add missing import in views
    
    * update msg trace
    
    - now formatted by django-bootstrap
    - change btn color
    
    * rename js block
    
    fix naming mismatch of javascript tag block
    
    * minor cleanup
    
    * formatting
    
    * formatting, linting
    
    * re-adding the magic run-config
    
    * formatted html
    
    * only deactivate plot button when plotform exists
    
    * Capitalize nouns/names
    
    * redirect from main index
    
    * remove outdated disabled state from buttons
    
    * update style of input
    
    * Move add name field
    
    * Change plot width
    
    * fix plot button when no plot form
    
    - should not delete token if token is not present
    
    * make history plots full width
    
    * change to offline bootstrap version
    
    - remove django-bootstrap-v5
    - include minified files
    - update requirements
    
    * fix django messages, make trace collapsible
    
    * Use method names from workflow meta
    
    * add semicolon and fix dynamic javascript
    
    * add bootstrap map for debugging
    
    * add run-name to title
    
    ---------
    
    Co-authored-by: antonneubauer <antongneubauer@gmail.com>

--- ui/main/settings.py
+++ ui/main/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "main.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "static" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
