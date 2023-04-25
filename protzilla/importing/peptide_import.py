5c0bc6d5b427dd64dfc00749db7e6390e07568b3
antonneubauer <antongneubauer@gmail.com>
2023-04-25 21:24:54 +0200

    peptide importer

--- protzilla/importing/peptide_import.py
+++ protzilla/importing/peptide_import.py
@@ -1,6 +1,40 @@
 from pathlib import Path
 
+import pandas as pd
+
 
 def peptide_import(_, file_path, intensity_name):
-    assert intensity_name in ["Intensity", "LFQ intensity"]
-    assert Path(file_path).is_file()
+    assert intensity_name in ["Intensity", "iBAQ", "LFQ intensity"]
+    assert Path(file_path).is_file(), f"Cannot find Peptide File at {file_path}"
+    # Intensity -> Intensity, iBAQ -> LFQ, LFQ -> LFQ
+    peptide_intensity_name = (
+        "LFQ intensity" if intensity_name == "iBAQ" else intensity_name
+    )
+
+    id_columns = ["Proteins", "Sequence", "PEP"]
+    read = pd.read_csv(
+        file_path,
+        sep="\t",
+        low_memory=False,
+        na_values=["", 0],
+        keep_default_na=True,
+    )
+
+    df = read.drop(columns=["Intensity"])
+    id_df = df[id_columns]
+    intensity_df = df.filter(regex=f"^{peptide_intensity_name} ", axis=1)
+    intensity_df.columns = [c[len(intensity_name) + 1 :] for c in intensity_df.columns]
+    molten = pd.melt(
+        pd.concat([id_df, intensity_df], axis=1),
+        id_vars=id_columns,
+        var_name="Sample",
+        value_name=peptide_intensity_name,
+    )
+
+    molten = molten.rename(columns={"Proteins": "Protein ID"})
+    ordered = molten[
+        ["Sample", "Protein ID", "Sequence", peptide_intensity_name, "PEP"]
+    ]
+    ordered.sort_values(by=["Sample", "Protein ID"], ignore_index=True, inplace=True)
+
+    return ordered, {}
