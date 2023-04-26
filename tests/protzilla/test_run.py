482efc6391901df05e17f2b92e6f96adc11d30ef
Fynn <fynn_kroeger@web.de>
2023-04-26 17:52:58 +0200

    add run.df again to tests

--- tests/protzilla/test_run.py
+++ tests/protzilla/test_run.py
@@ -41,17 +41,16 @@ def test_run_back():
         file_path=f"{PROJECT_PATH}/tests/proteinGroups_small_cut.txt",
         intensity_name="Intensity",
     )
-    df1 = run.history.steps[-1].dataframe
+    df1 = run.df
     run.calculate_and_next(
         data_preprocessing.filter_proteins.by_low_frequency, threshold=1
     )
-    df2 = run.history.steps[-1].dataframe
+    df2 = run.df
     assert not df1.equals(df2)
     run.back_step()
-    df3 = run.history.steps[-1].dataframe
-    assert df3.equals(df1)
+    assert run.df.equals(df1)
     run.back_step()
-    assert not run.history.steps
+    assert run.df is None
     rmtree(RUNS_PATH / name)
 
 
@@ -64,10 +63,10 @@ def test_run_continue():
         file_path=f"{PROJECT_PATH}/tests/proteinGroups_small_cut.txt",
         intensity_name="Intensity",
     )
-    df = run.history.steps[-1].dataframe
+    df = run.df
     del run
     run2 = Run.continue_existing(run_name)
-    assert df.equals(run2.history.steps[-1].dataframe)
+    assert df.equals(run2.df)
     rmtree(RUNS_PATH / run_name)
 
 
