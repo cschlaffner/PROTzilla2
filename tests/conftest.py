2f496688a57ce1a06d8ade7bd727aed2abc501c8
Sara Grau <sara.grau-i-blade@student.hpi.uni-potsdam.de>
2023-04-27 15:30:15 +0200

    shortened df_with_nan fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -65,21 +65,6 @@ def df_with_nan():
         ["Sample2", "Protein1", "Gene1", np.nan],
         ["Sample2", "Protein2", "Gene1", 18],
         ["Sample2", "Protein3", "Gene1", 2],
-        ["Sample3", "Protein1", "Gene1", 22],
-        ["Sample3", "Protein2", "Gene1", 19],
-        ["Sample3", "Protein3", "Gene1", 3],
-        ["Sample4", "Protein1", "Gene1", 8],
-        ["Sample4", "Protein2", "Gene1", np.nan],
-        ["Sample4", "Protein3", "Gene1", 1],
-        ["Sample5", "Protein1", "Gene1", 10],
-        ["Sample5", "Protein2", "Gene1", 14],
-        ["Sample5", "Protein3", "Gene1", 2],
-        ["Sample6", "Protein1", "Gene1", 12],
-        ["Sample6", "Protein2", "Gene1", 13],
-        ["Sample6", "Protein3", "Gene1", 3],
-        ["Sample7", "Protein1", "Gene1", 12],
-        ["Sample7", "Protein2", "Gene1", 13],
-        ["Sample7", "Protein3", "Gene1", 3],
     )
 
     df_with_nan = pd.DataFrame(
