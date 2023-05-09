e2eb215685b43dcedee319127d734a665ca9093e
antonneubauer <antongneubauer@gmail.com>
2023-05-09 19:49:38 +0200

    adding warning in case protein-iterable is unexpected type

--- ui/runs/views.py
+++ ui/runs/views.py
@@ -150,6 +150,9 @@ def change_field(request, run_name):
                 param_dict["categories"] = protein_itr
             else:
                 param_dict["categories"] = []
+                print(
+                    f"Warning: expected protein_itr to be a DataFrame, Series or list, but got {type(protein_itr)}"
+                )
 
         fields[key] = make_parameter_input(key, param_dict, disabled=False)
 
