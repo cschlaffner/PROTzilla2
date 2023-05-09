771292e2512a9c6ec938c5f4a3da47953aab6bd3
antonneubauer <antongneubauer@gmail.com>
2023-05-09 10:56:42 +0200

    Unix Install script
    * basic install & run-scripts for unix
    
    * removed unused files
    
    * Fix runner2 (#150)
    
    * added default named_outputs
    
    * fixed runner
    
    * autoformatting
    
    * isort
    
    * added integration test for runner
    
    * fix test
    
    * adopted requested changes
    
    * fixed path of test
    
    * fixed error on default not in options
    
     fixed problem with not existing defaults in options
    
    * added autoselecting groups in ttest
    
    * split script into env, conda, run
    
    * remove debugging
    
    * more better things
    
    * running
    
    ---------
    
    Co-authored-by: Fynn <fynn_kroeger@web.de>
    Co-authored-by: brokkoli71 <44113112+brokkoli71@users.noreply.github.com>

---
+++ install_scripts/create_env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if ! [[ "$OSTYPE" == "linux-gnu"* ]] && ! [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "OS not supported, use the install_windows.bat script (to be written)."
+  exit 1
+fi
+
+ENV_NAME="protzilla"
+
+# Check if the environment exists
+if conda info --envs | grep -q "$ENV_NAME"; then
+  echo "$ENV_NAME environment already exists."
+else
+  # Create a new environment
+  echo "Creating $ENV_NAME environment..."
+  conda create -y --name $ENV_NAME python=3.11
+fi
+
+# Activate the new environment
+eval "$(conda shell.bash hook)"
+conda activate $ENV_NAME
+
+# Check if the environment is activated, for debugging purposes
+# conda info --envs | grep "$ENV_NAME"; exit 1
+
+# Install the requirements using pip
+echo "Installing requirements..."
+pip install -q -r requirements.txt
