dea5855103440789d6a350c79647e3f5d7065ab3
brokkoli71 <brokkoli71b@gmail.com>
2023-05-09 11:21:17 +0200

    windows batch

--- run_protzilla.bat
+++ run_protzilla.bat
@@ -1,18 +1,21 @@
 @echo off
+echo Welcome to PROTzilla
 
-REM Check if Anaconda is installed
-conda --version >nul 2>&1
+REM Check if Anaconda or Miniconda3 is installed
+call conda --version >nul 2>&1
 if %errorlevel% EQU 0 (
-    echo Anaconda is already installed.
+    echo conda already installed
     goto check_environment
 )
 
+echo Anaconda not already installed.
 REM Check if Miniconda3 is installed
 miniconda --version >nul 2>&1
 if %errorlevel% EQU 0 (
     echo Miniconda3 is already installed.
     goto check_environment
 )
+echo Miniconda3 not already installed.
 
 REM Install Miniconda3
 echo Installing Miniconda3...
@@ -33,13 +36,16 @@ if %errorlevel% EQU 0 (
 
 REM Create the environment
 echo Creating protzilla_win environment...
-conda create --name protzilla_win
+call conda create -y --name protzilla_win python=3.11
 
 echo protzilla_win environment created.
 
 :activate_env
-REM Activate the environment
+REM Activate the environment and install requirements
 echo Activating protzilla_win environment...
+python --version
 call activate protzilla_win
-
-echo protzilla_win environment activated.
+python --version
+pip install -r requirements.txt
+REM Run Django server
+python ui/manage.py runserver
