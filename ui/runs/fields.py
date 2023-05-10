cf7fca43b9222583e8dffd92f0e99c5904235486
Sara Grau <sara.grau-i-blade@student.hpi.uni-potsdam.de>
2023-05-10 17:37:28 +0200

    added protein reference field

--- ui/runs/fields.py
+++ ui/runs/fields.py
@@ -43,6 +43,8 @@ def make_parameter_input(key, param_dict, disabled):
         template = "runs/field_named.html"
     elif param_dict["type"] == "metadata_df":
         template = "runs/field_empty.html"
+    elif param_dict["type"] == "text":
+        template = "runs/field_plain_text.html"
     else:
         raise ValueError(f"cannot match parameter type {param_dict['type']}")
 
