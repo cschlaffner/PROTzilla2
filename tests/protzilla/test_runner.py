f8e41525a5fd380fdea14d2366aaf05665ba5e18
antonneubauer <antongneubauer@gmail.com>
2023-05-12 15:21:10 +0200

    adapting tests to include peptides-path

--- tests/protzilla/test_runner.py
+++ tests/protzilla/test_runner.py
@@ -190,12 +190,13 @@ def test_serialize_workflow_graphs():
 
 
 def test_integration_runner(metadata_path, ms_data_path, tests_folder_name):
-    name = tests_folder_name + "/test_runner_integration" + random_string()
+    name = tests_folder_name + "/test_runner_integration_" + random_string()
     runner = Runner(
         **{
             "workflow": "standard",
             "ms_data_path": f"{PROJECT_PATH}/{ms_data_path}",
             "meta_data_path": f"{PROJECT_PATH}/{metadata_path}",
+            "peptides_path": None,
             "run_name": f"{name}",
             "df_mode": "disk",
             "all_plots": True,
@@ -212,6 +213,7 @@ def test_integration_runner_no_plots(metadata_path, ms_data_path, tests_folder_n
             "workflow": "standard",
             "ms_data_path": f"{PROJECT_PATH}/{ms_data_path}",
             "meta_data_path": f"{PROJECT_PATH}/{metadata_path}",
+            "peptides_path": None,
             "run_name": f"{name}",
             "df_mode": "disk",
             "all_plots": False,
