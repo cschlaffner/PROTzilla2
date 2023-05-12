05523ed2329398ac52fb98e1e57cf2827b5fbddd
antonneubauer <antongneubauer@gmail.com>
2023-05-12 13:47:21 +0200

    show error message in frontend

--- protzilla/importing/peptide_import.py
+++ protzilla/importing/peptide_import.py
@@ -1,11 +1,22 @@
 from pathlib import Path
 
 import pandas as pd
+from django.contrib import messages
 
 
 def peptide_import(ms_df, file_path, intensity_name):
-    assert intensity_name in ["Intensity", "iBAQ", "LFQ intensity"]
-    assert Path(file_path).is_file(), f"Cannot find Peptide File at {file_path}"
+    try:
+        assert intensity_name in [
+            "Intensity",
+            "iBAQ",
+            "LFQ intensity",
+        ], f"Unknown intensity name: {intensity_name}"
+        assert Path(file_path).is_file(), f"Cannot find Peptide File at {file_path}"
+    except AssertionError as e:
+        return ms_df, dict(
+            peptide_df=None, messages=[dict(level=messages.ERROR, msg=e)]
+        )
+
     # Intensity -> Intensity, iBAQ -> LFQ, LFQ -> LFQ
     peptide_intensity_name = (
         "LFQ intensity" if intensity_name == "iBAQ" else intensity_name
@@ -37,4 +48,4 @@ def peptide_import(ms_df, file_path, intensity_name):
     ]
     ordered.sort_values(by=["Sample", "Protein ID"], ignore_index=True, inplace=True)
 
-    return ms_df, {"peptide_df": ordered}
+    return ms_df, dict(peptide_df=ordered)
