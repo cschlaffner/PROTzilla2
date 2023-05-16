da73628a22a863c8ced91a0b3abc6b99315b5593
brokkoli71 <brokkoli71b@gmail.com>
2023-05-16 14:43:22 +0200

    added by_proteins_missing

--- tests/protzilla/data_preprocessing/test_filter_samples.py
+++ tests/protzilla/data_preprocessing/test_filter_samples.py
@@ -6,6 +6,8 @@ from protzilla.data_preprocessing.filter_samples import (
     by_protein_count_plot,
     by_protein_intensity_sum,
     by_protein_intensity_sum_plot,
+    by_proteins_missing,
+    by_proteins_missing_plot,
 )
 
 
@@ -17,33 +19,13 @@ def filter_samples_df():
         ["Sample1", "Protein3", "Gene3", 3],
         ["Sample1", "Protein4", "Gene4", 4],
         ["Sample1", "Protein5", "Gene5", 5],
-        [
-            "Sample2",
-            "Protein1",
-            "Gene1",
-        ],
+        ["Sample2", "Protein1", "Gene1"],
         ["Sample2", "Protein2", "Gene2", 2],
-        [
-            "Sample2",
-            "Protein3",
-            "Gene3",
-        ],
+        ["Sample2", "Protein3", "Gene3"],
         ["Sample2", "Protein4", "Gene4", 5],
-        [
-            "Sample2",
-            "Protein5",
-            "Gene5",
-        ],
-        [
-            "Sample3",
-            "Protein1",
-            "Gene1",
-        ],
-        [
-            "Sample3",
-            "Protein2",
-            "Gene2",
-        ],
+        ["Sample2", "Protein5", "Gene5"],
+        ["Sample3", "Protein1", "Gene1"],
+        ["Sample3", "Protein2", "Gene2"],
         ["Sample3", "Protein3", "Gene3", 10],
         ["Sample3", "Protein4", "Gene4", 20],
         ["Sample3", "Protein5", "Gene5", 20],
@@ -58,6 +40,38 @@ def filter_samples_df():
     )
 
     return filter_samples_df
+
+
+def test_by_proteins_missing(filter_samples_df, show_figures):
+    result_df1, dropouts1 = by_proteins_missing(filter_samples_df, percentage=0.5)
+    result_df2, dropouts2 = by_proteins_missing(filter_samples_df, percentage=0.6)
+    result_df3, dropouts3 = by_proteins_missing(filter_samples_df, percentage=0.65)
+
+    list_proteins_excluded_1 = dropouts1["filtered_samples"]
+    list_proteins_excluded_2 = dropouts2["filtered_samples"]
+    list_proteins_excluded_3 = dropouts3["filtered_samples"]
+
+    fig = by_proteins_missing_plot(filter_samples_df, result_df1, dropouts1, "Pie chart")[
+        0
+    ]
+    if True:
+        fig.show()
+
+    assert [
+        "Sample2",
+    ] == list_proteins_excluded_1, f"excluded proteins do not match \
+                Protein1 and Protein4, but are {list_proteins_excluded_1}"
+
+    assert [
+        "Sample2",
+        "Sample3",
+    ] == list_proteins_excluded_2, f"excluded proteins do not match \
+                Protein1 and Protein4, but are {list_proteins_excluded_2}"
+    assert [
+            "Sample2",
+            "Sample3",
+    ] == list_proteins_excluded_3, f"excluded proteins do not match \
+                    Protein1 and Protein4, but are {list_proteins_excluded_2}"
 
 
 def test_filter_samples_by_protein_count(filter_samples_df, show_figures):
