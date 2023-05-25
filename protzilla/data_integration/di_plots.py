ba50f4c56a68c064387b8407f34ecc09ea9dab12
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2023-05-25 16:01:31 +0200

    reformat, documentation, user safe

--- protzilla/data_integration/di_plots.py
+++ protzilla/data_integration/di_plots.py
@@ -9,6 +9,7 @@ from ..constants.colors import PROTZILLA_DISCRETE_COLOR_SEQUENCE
 
 from shutil import get_terminal_size
 
+
 def go_enrichment_bar_plot(
     input_df,
     categories,
@@ -17,12 +18,36 @@ def go_enrichment_bar_plot(
     title="",
     colors=PROTZILLA_DISCRETE_COLOR_SEQUENCE,
 ):
-    pd.set_option('display.width', get_terminal_size()[0])
+    """
+    Create a bar plot for the GO enrichment results. The plot is created using the gseapy library.
+    Groups the bars by the enrichment categories (e.g. KEGG, Reactome, etc.) and sorts the bars by
+    the adjusted p-value or FDR, whichever is available in the input data. A cutoff can be applied
+    to the data to only show significant results or results with a low FDR.
+
+    :param input_df: GO enrichment results
+    :type input_df: pandas.DataFrame
+    :param categories: Categories/Sets from enrichment to plot
+    :type categories: list
+    :param top_terms: Number of top enriched terms per category
+    :type top_terms: int
+    :param cutoff: Cutoff for the Adjusted p-value or FDR. Only terms with
+        Adjusted P-value (or FDR) < cutoff will be shown.
+    :type cutoff: float
+    :param title: Title of the plot, defaults to ""
+    :type title: str, optional
+    :param colors: Colors to use for the bars, defaults to PROTZILLA_DISCRETE_COLOR_SEQUENCE
+    :type colors: list, optional
+    :return: Base64 encoded image of the plot
+    :rtype: bytes
+    """
 
     if input_df is None or len(input_df) == 0 or input_df.empty:
         msg = "No data to plot. Please check your input data or run enrichment again."
         return [dict(messages=[dict(level=messages.ERROR, msg=msg)])]
-    
+
+    if not isinstance(categories, list):
+        categories = [categories]
+
     # remove all Gene_sets that are not in categories
     df = input_df[input_df["Gene_set"].isin(categories)]
 
@@ -33,13 +58,12 @@ def go_enrichment_bar_plot(
     if "score" in df.columns and "ID" in df.columns:
         # df is a restring summary file
         restring_input = True
-        df = df.rename(columns={"ID": "Term"})  
+        df = df.rename(columns={"ID": "Term"})
         fdr_column = "score"
     elif "term" in df.columns and "common" in df.columns:
         # df is a restring results file
         restring_input = True
         df = df.rename(columns={"term": "Term"})
-        print(df.columns)
         fdr_column = df.columns[2]
 
     if restring_input:
@@ -48,30 +72,42 @@ def go_enrichment_bar_plot(
         if len(df) == 0:
             msg = f"No data to plot when applying cutoff {cutoff}. Check your input data or choose a different cutoff."
             return [dict(messages=[dict(level=messages.WARNING, msg=msg)])]
-        
+
         column = "-log10(FDR)"
         df[column] = -1 * np.log10(df[fdr_column])
+        # prevent cutoff being applied again
         cutoff = df[column].max()
         df["Overlap"] = "0/0"
     else:
         column = "Adjusted P-value"
 
     size_y = top_terms * 0.5 * len(categories)
-    # ValueError: Warning: No enrich terms when cutoff = 0
-    ax = gp.barplot(
-        df=df,
-        column=column,
-        cutoff=cutoff,
-        group="Gene_set",
-        figsize=(10, size_y),
-        top_term=top_terms,
-        color=colors,
-        title=title,
-    )
+    try:
+        ax = gp.barplot(
+            df=df,
+            column=column,
+            cutoff=cutoff,
+            group="Gene_set",
+            figsize=(10, size_y),
+            top_term=top_terms,
+            color=colors,
+            title=title,
+        )
+    except ValueError as e:
+        msg = f"No data to plot when applying cutoff {cutoff}. Check your input data or choose a different cutoff."
+        return [dict(messages=[dict(level=messages.ERROR, msg=msg, trace=str(e))])]
     return [fig_to_base64(ax.get_figure())]
 
 
 def fig_to_base64(fig):
+    """
+    Convert a matplotlib figure to base64. This is used to display the figure in the browser.
+
+    :param fig: matplotlib figure
+    :type fig: matplotlib.figure.Figure
+    :return: base64 encoded image
+    :rtype: bytes
+    """
     img = io.BytesIO()
     fig.savefig(img, format="png", bbox_inches="tight")
     img.seek(0)
--- tests/protzilla/data_integration/test_plots_data_integration.py
+++ tests/protzilla/data_integration/test_plots_data_integration.py
@@ -6,6 +6,7 @@ from PIL import Image
 from protzilla.constants.paths import PROJECT_PATH
 from protzilla.data_integration.di_plots import *
 
+
 def open_graph_from_base64(encoded_string):
     decoded_bytes = base64.b64decode(encoded_string)
     image_stream = io.BytesIO(decoded_bytes)
@@ -17,33 +18,35 @@ def test_enrichment_bar_plot_restring(show_figures):
     test_data_folder = f"{PROJECT_PATH}/tests/test_data/enrichment_data"
     result = pd.read_csv(f"{test_data_folder}/merged_results.csv", header=0)
     bar_base64 = go_enrichment_bar_plot(
-            input_df=result,
-            categories=["KEGG", "Process"],
-            top_terms=10,
-            cutoff=0.05,
-        )
+        input_df=result,
+        categories=["KEGG", "Process"],
+        top_terms=10,
+        cutoff=0.05,
+    )
     if show_figures:
         open_graph_from_base64(bar_base64[0])
 
     summary = pd.read_csv(f"{test_data_folder}/merged_summaries.csv", header=0)
     bar_base64 = go_enrichment_bar_plot(
-            input_df=summary,
-            categories=["KEGG", "Process"],
-            top_terms=10,
-            cutoff=0.05,
-        )
+        input_df=summary,
+        categories=["KEGG", "Process"],
+        top_terms=10,
+        cutoff=0.05,
+    )
     if show_figures:
         open_graph_from_base64(bar_base64[0])
 
 
 def test_enrichment_bar_plot(show_figures):
     test_data_folder = f"{PROJECT_PATH}/tests/test_data/enrichment_data"
-    enrichment_df = pd.read_csv(f"{test_data_folder}/Reactome_enrichment_enrichr.csv", header=0)
+    enrichment_df = pd.read_csv(
+        f"{test_data_folder}/Reactome_enrichment_enrichr.csv", header=0
+    )
     bar_base64 = go_enrichment_bar_plot(
-            input_df=enrichment_df,
-            categories=["Reactome_2013"],
-            top_terms=10,
-            cutoff=0.05,
-        )
+        input_df=enrichment_df,
+        categories=["Reactome_2013"],
+        top_terms=10,
+        cutoff=0.05,
+    )
     if show_figures:
         open_graph_from_base64(bar_base64[0])
