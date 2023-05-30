814e069e8f3334ef15a7996cbe2315092642423a
Lilly Zintl <Lilly.Zintl@student.hpi.de>
2023-05-30 17:40:35 +0200

    Add test for filter proteins by missing samples

--- tests/protzilla/data_preprocessing/test_filter_proteins.py
+++ tests/protzilla/data_preprocessing/test_filter_proteins.py
@@ -5,6 +5,8 @@ import pytest
 from protzilla.data_preprocessing.filter_proteins import (
     by_low_frequency,
     by_low_frequency_plot,
+    by_samples_missing,
+    by_samples_missing_plot,
 )
 
 
@@ -39,6 +41,34 @@ def filter_proteins_df():
     return filter_proteins_df
 
 
+def filter_proteins_by_perentage_df():
+    filter_proteins_df = pd.DataFrame(
+        (
+            ["Sample1", "Protein1", "Gene1", 1],
+            ["Sample1", "Protein2", "Gene1", np.nan],
+            ["Sample1", "Protein3", "Gene1", np.nan],
+            ["Sample1", "Protein4", "Gene1", np.nan],
+            ["Sample1", "Protein5", "Gene1", np.nan],
+            ["Sample2", "Protein1", "Gene1", 1],
+            ["Sample2", "Protein2", "Gene1", 1],
+            ["Sample2", "Protein3", "Gene1", np.nan],
+            ["Sample2", "Protein4", "Gene1", np.nan],
+            ["Sample2", "Protein5", "Gene1", np.nan],
+            ["Sample3", "Protein1", "Gene1", 1],
+            ["Sample3", "Protein2", "Gene1", 1],
+            ["Sample3", "Protein3", "Gene1", 1],
+            ["Sample3", "Protein4", "Gene1", np.nan],
+            ["Sample3", "Protein5", "Gene1", np.nan],
+            ["Sample4", "Protein1", "Gene1", 1],
+            ["Sample4", "Protein2", "Gene1", 1],
+            ["Sample4", "Protein3", "Gene1", 1],
+            ["Sample4", "Protein4", "Gene1", 1],
+            ["Sample4", "Protein5", "Gene1", np.nan],
+        ),
+        columns=["Sample", "Protein ID", "Gene", "Intensity"],
+    )
+
+
 def test_filter_proteins_by_low_frequency(filter_proteins_df, show_figures):
     result_df, dropouts = by_low_frequency(filter_proteins_df, threshold=0.6)
     list_proteins_excluded = dropouts["filtered_proteins"]
@@ -52,3 +82,25 @@ def test_filter_proteins_by_low_frequency(filter_proteins_df, show_figures):
         "Protein4",
     ] == list_proteins_excluded, f"excluded proteins do not match \
             Protein1 and Protein4, but are {list_proteins_excluded}"
+
+
+def test_filter_proteins_by_percentage(filter_proteins_df, show_figures):
+    result_df, dropouts = by_samples_missing(filter_proteins_df, threshold=1.0)
+    list_proteins_excluded = dropouts["filtered_proteins"]
+
+    fig = by_samples_missing_plot(filter_proteins_df, result_df, dropouts, "Pie chart")[
+        0
+    ]
+    if show_figures:
+        fig.show()
+    assert ["Protein2", "Protein3", "Protein4", "Protein5"] == list_proteins_excluded
+
+    result_df, dropouts = by_samples_missing(filter_proteins_df, threshold=0.5)
+    list_proteins_excluded = dropouts["filtered_proteins"]
+
+    assert ["Protein4", "Protein5"] == list_proteins_excluded
+
+    result_df, dropouts = by_samples_missing(filter_proteins_df, threshold=0.0)
+    list_proteins_excluded = dropouts["filtered_proteins"]
+
+    assert [] == list_proteins_excluded
