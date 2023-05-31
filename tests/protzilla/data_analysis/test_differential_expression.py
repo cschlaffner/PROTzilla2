9cb41a4ffd09304187a2f02cd169801619d2b5f7
brokkoli71 <brokkoli71b@gmail.com>
2023-05-31 17:21:53 +0200

    test linear_model

--- tests/protzilla/data_analysis/test_differential_expression.py
+++ tests/protzilla/data_analysis/test_differential_expression.py
@@ -1,9 +1,105 @@
 import numpy as np
 import pandas as pd
+import pytest
 
 from protzilla.data_analysis.differential_expression import anova, t_test
 from protzilla.data_analysis.differential_expression_linear_model import linear_model
 from protzilla.data_analysis.plots import create_volcano_plot
+
+
+@pytest.fixture
+def data_test_differential_expression():
+    test_intensity_list = (
+        ["Sample1", "Protein1", "Gene1", 18],
+        ["Sample1", "Protein2", "Gene1", 16],
+        ["Sample1", "Protein3", "Gene1", 1],
+        ["Sample2", "Protein1", "Gene1", 20],
+        ["Sample2", "Protein2", "Gene1", 15],
+        ["Sample2", "Protein3", "Gene1", 2],
+        ["Sample3", "Protein1", "Gene1", 22],
+        ["Sample3", "Protein2", "Gene1", 14],
+        ["Sample3", "Protein3", "Gene1", 3],
+        ["Sample4", "Protein1", "Gene1", 8],
+        ["Sample4", "Protein2", "Gene1", 15],
+        ["Sample4", "Protein3", "Gene1", 1],
+        ["Sample5", "Protein1", "Gene1", 10],
+        ["Sample5", "Protein2", "Gene1", 14],
+        ["Sample5", "Protein3", "Gene1", 2],
+        ["Sample6", "Protein1", "Gene1", 12],
+        ["Sample6", "Protein2", "Gene1", 13],
+        ["Sample6", "Protein3", "Gene1", 3],
+        ["Sample7", "Protein1", "Gene1", 12],
+        ["Sample7", "Protein2", "Gene1", 13],
+        ["Sample7", "Protein3", "Gene1", 3],
+    )
+
+    test_intensity_df = pd.DataFrame(
+        data=test_intensity_list,
+        columns=["Sample", "Protein ID", "Gene", "Intensity"],
+    )
+
+    test_metadata_list = (
+        ["Sample1", "Group1"],
+        ["Sample2", "Group1"],
+        ["Sample3", "Group1"],
+        ["Sample4", "Group2"],
+        ["Sample5", "Group2"],
+        ["Sample6", "Group2"],
+        ["Sample7", "Group3"],
+    )
+
+    test_metadata_df = pd.DataFrame(
+        data=test_metadata_list,
+        columns=["Sample", "Group"],
+    )
+    return test_intensity_df, test_metadata_df
+
+
+def test_differential_expression_linear_model(
+    data_test_differential_expression,
+    show_figures,
+):
+    test_intensity_df, test_metadata_df = data_test_differential_expression
+    test_alpha = 0.05
+    test_fc_threshold = 0
+
+    current_out = linear_model(
+        test_intensity_df,
+        test_metadata_df,
+        grouping="Group",
+        group1="Group1",
+        group2="Group2",
+        multiple_testing_correction_method="Benjamini-Hochberg",
+        alpha=test_alpha,
+        fc_threshold=test_fc_threshold,
+    )
+
+    fig = create_volcano_plot(
+        current_out["corrected_p_values_df"],
+        current_out["log2_fold_change_df"],
+        current_out["fc_threshold"],
+        current_out["alpha"],
+    )
+    if show_figures:
+        fig.show()
+
+    corrected_p_values = [0.0108, 0.4318, 1.000]
+    log2_fc = [-5, -0.5, 0]
+    de_proteins = ["Protein1"]
+
+    p_values_rounded = [
+        round(x, 4) for x in current_out["corrected_p_values_df"]["corrected_p_value"]
+    ]
+    log2fc_rounded = [
+        round(x, 4) for x in current_out["log2_fold_change_df"]["log2_fold_change"]
+    ]
+
+    assert p_values_rounded == corrected_p_values
+    assert log2fc_rounded == log2_fc
+    assert current_out["de_proteins_df"]["Protein ID"].unique() == de_proteins
+    assert current_out["fc_threshold"] == test_fc_threshold
+    assert current_out["alpha"] == test_alpha
+    assert current_out["corrected_alpha"] is None
 
 
 def test_differential_expression_t_test(show_figures):
@@ -54,7 +150,7 @@ def test_differential_expression_t_test(show_figures):
     test_alpha = 0.05
     test_fc_threshold = 0
 
-    current_out = linear_model(
+    current_out = t_test(
         test_intensity_df,
         test_metadata_df,
         grouping="Group",
