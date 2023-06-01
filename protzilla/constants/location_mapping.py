4a815838f03b52773a97cd8773743cad6665d535
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2023-06-01 15:16:54 +0200

    Bar plot for GO enrichment analyses (#191)
    * create branch and methods
    
    * WIP enrichment analysis
    
    * WIP enrichment with STRING
    
    * basic STRING function
    
    * WIP analysis with string
    
    * WIP enrichment analysis
    
    * WIP go_analysis_with_STRING
    
    * minor cleanup
    
    * include gseapy dbs
    
    * read protein sets file
    
    * add background for enrichr offline
    
    * add online enrichment analysis
    
    * switch restring version
    
    * get_functional_enrichment_with_delay
    
    * add first tests
    
    * remove cutoff
    
    will be graph param
    
    * add more input tests
    
    * test get_functional_enrichment_with_delay
    
    * write test_go_analysis_with_enrichr test
    
    * WIP testing
    
    * WIP testing
    
    * fix deleting tmp folder
    
    * make test_go_analysis_with_STRING work
    
    * reformat
    
    * add further go_analysis_with_STRING tests
    
    * change test order
    
    * finish test
    
    * remove RUNS_PATH
    
    * update peptide df type
    
    * fix workflow issues
    
    * reformat
    
    * add documentation
    
    * delete other methods from other branch
    
    * reformat with black
    
    * Update requirements.txt
    
    * add documentation to go analysis online
    
    * add uniprot_ids_to_uppercase_gene_symbols() functions
    
    * test go analysis with enrichr
    
    * reformat
    
    * fix test
    
    * reformat
    
    * add runconfig back
    
    * WIP plots
    
    * working bar plot & reformat
    
    * fix plot layout
    
    * make named dynamic inputs work
    
    * Handle no protein sets chosen
    
    * handle wrong organism id
    
    * format and restructure tests
    
    * simplify tests
    
    * merge String dfs
    
    * tests and make work for restring
    
    * reformat, documentation, user safe
    
    * delete duplicate methods
    
    * cleanup
    
    * Update enrichment_analysis.py
    
    * implement comments
    
    * remove error when changing input df twice
    
    * explain file types in more detail
    
    * implement review
    
    * set default value for categories and changed position of error handling
    
    * fixed failed test
    
    ---------
    
    Co-authored-by: Sara Grau <sara.grau-i-blade@student.hpi.uni-potsdam.de>

--- protzilla/constants/location_mapping.py
+++ protzilla/constants/location_mapping.py
@@ -1,3 +1,4 @@
+from ..data_integration import enrichment_analysis, di_plots
 from ..data_analysis import (
     clustering,
     differential_expression,
@@ -284,4 +285,9 @@ plot_map = {
         "plot",
         "clustergram",
     ): plots.clustergram_plot,
+    (
+        "data_integration",
+        "plot",
+        "go_enrichment_bar_plot",
+    ): di_plots.go_enrichment_bar_plot,
 }
