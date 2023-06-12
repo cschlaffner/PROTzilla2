47e407e889894a39ffec4d5bde8b532af8008917
Fynn <fynn_kroeger@web.de>
2023-06-12 20:51:08 +0200

    order protein groups, show results in order

--- protzilla/data_integration/database_integration.py
+++ protzilla/data_integration/database_integration.py
@@ -1,6 +1,17 @@
 import pandas as pd
+from itertools import groupby
+import operator
 
 from protzilla.data_integration.database_query import uniprot_query_dataframe
+from protzilla.importing.ms_data_import import normalize_uniprot_id
+
+
+# recipie from https://docs.python.org/3/library/itertools.html
+def unique_justseen(iterable, key=None):
+    """List unique elements, preserving order. Remember only the element just seen."""
+    # unique_justseen('AAAABBBCCDAABBB') --> A B C D A B
+    # unique_justseen('ABBcCAD', str.lower) --> A B c A D
+    return map(next, map(operator.itemgetter(1), groupby(iterable, key)))
 
 
 def add_uniprot_data(dataframe, fields=None):
@@ -19,36 +30,38 @@ def add_uniprot_data(dataframe, fields=None):
     # remove isoforms and variants from groups
     for group in groups:
         proteins = group.split(";")
-        cleaned = set()
+        cleaned = []
         for protein in proteins:
-            if "-" in protein:
-                protein = protein.split("-")[0]
-            if protein.startswith("CON__") or protein.startswith("REV__"):
-                protein = protein[5:]
-            if "_VAR_" in protein:
-                protein = protein.split("_VAR_")[0]
-            cleaned.add(protein)
-            all_proteins.add(protein)
-        clean_groups.append(cleaned)
+            normalized = normalize_uniprot_id(protein)
+            cleaned.append(normalized)
+            all_proteins.add(normalized)
+        # this can be done because we make isoforms appear together
+        clean_groups.append(list(unique_justseen(cleaned)))
     res: pd.DataFrame = uniprot_query_dataframe(list(all_proteins), fields)
 
     new_columns = {k: [] for k in fields}  # data that will be added to input df
+    # is it better to have a for loop for columns and retrive each protein multiple times from res?
+
     for group in clean_groups:
         group_dict = {k: [] for k in fields}
         # data that different members of group have
         for member in group:
             if member not in res.index:
-                continue
-            result = res.loc[member]
-            for col, val in result.to_dict().items():
-                if "Gene Names" in col and not isinstance(val, float):
+                result = {k: None for k in fields}
+            else:
+                result = res.loc[member].to_dict()
+            for col, val in result.items():
+                if "Gene Names" in col and isinstance(val, str):
                     # to remove space seperated groupings
                     group_dict[col].append(val.split()[0])
                 else:
                     group_dict[col].append(val)
         for field in fields:
             # add unique values to only, join if necessary
-            new_columns[field].append(";".join(map(str, set(group_dict[field]))))
+            if len(set(group_dict[field])) == 1:
+                new_columns[field].append(group_dict[field][0])
+            else:
+                new_columns[field].append(";".join(map(str, group_dict[field])))
 
     # we can concat because clean groups has the same order as groups
     out = pd.concat([dataframe, pd.DataFrame(new_columns)], axis=1)
@@ -57,11 +70,12 @@ def add_uniprot_data(dataframe, fields=None):
 
 if __name__ == "__main__":
     df = pd.read_csv(
-        "/Users/fynnkroeger/Desktop/Studium/Bachelorprojekt/PROTzilla2/user_data/runs/adfg/history_dfs/8-data_analysis-differential_expression-t_test-log2_fold_change_df.csv",
+        "/Users/fynnkroeger/Desktop/Studium/Bachelorprojekt/PROTzilla2/user_data/runs/with_id_sorting/history_dfs/8-data_analysis-differential_expression-t_test-log2_fold_change_df.csv",
         index_col=0,
     )
     from time import time
 
     t = time()
-    add_uniprot_data(df, fields=["Gene Names", "Organism", "Length"])
+    a = add_uniprot_data(df, fields=["Gene Names (primary)", "Organism", "Length"])
+    a["result"].to_csv("result.csv")
     print(time() - t)
