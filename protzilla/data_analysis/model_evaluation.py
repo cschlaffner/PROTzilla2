93bed4507a69f75b1d1d34858c5fa8f5f75fa03a
Sara Grau <sara.grau-i-blade@student.hpi.uni-potsdam.de>
2023-06-13 14:46:28 +0200

    added docstrings

--- protzilla/data_analysis/model_evaluation.py
+++ protzilla/data_analysis/model_evaluation.py
@@ -7,6 +7,21 @@ from protzilla.data_analysis.classification_helper import (
 
 
 def evaluate_classification_model(model, input_test_df, labels_test_df, scoring):
+    """Function that asseses an already trained classification model on separate testing
+    data using widely used scoring metrics
+
+    :param model: The trained classification model instance to be evaluated.
+    :type model: BaseEstimator
+    :param input_test_df: The input features of the testing data as a DataFrame.
+    :type input_test_df: pd.DataFrame
+    :param labels_test_df: The true labels of the testing data as a DataFrame.
+    :type labels_test_df: pd.DataFrame
+    :param scoring: The scoring metric to be used for evaluation. It can be a string
+     representing a predefined metric.
+    :type scoring: str or callable
+    :return: A dataframe with the metric name and its corresponding score.
+    :rtype: dict
+    """
     input_test_df = input_test_df.set_index("Sample")
     label_encoder, y_test_encoded = encode_labels(
         input_test_df, labels_test_df, "Label"
--- protzilla/data_analysis/model_evaluation_plots.py
+++ protzilla/data_analysis/model_evaluation_plots.py
@@ -7,6 +7,19 @@ from protzilla.utilities.transform_figure import fig_to_base64
 
 
 def precision_recall_curve(model, input_test_df, labels_test_df, plot_title=None):
+    """
+    Calculate and plot the precision-recall curve for a classification model.
+    :param model: The trained classification model instance to be evaluated.
+    :type model: BaseEstimator
+    :param input_test_df: The input features of the testing data as a DataFrame.
+    :type input_test_df: pd.DataFrame
+    :param labels_test_df: The true labels of the testing data as a DataFrame.
+    :type labels_test_df: pd.DataFrame
+    :param plot_title: The title of the precision-recall curve plot. This is an optional
+     parameter.
+    :type plot_title: str, optional
+    :return: Base64 encoded image of the plot
+    :rtype: bytes"""
     input_test_df = input_test_df.set_index("Sample")
     label_encoder, y_test_encoded = encode_labels(
         input_test_df, labels_test_df, "Label"
@@ -26,6 +39,19 @@ def precision_recall_curve(model, input_test_df, labels_test_df, plot_title=None
 
 
 def roc_curve(model, input_test_df, labels_test_df, plot_title=None):
+    """
+    Calculate and plot the roc curve for a classification model.
+    :param model: The trained classification model instance to be evaluated.
+    :type model: BaseEstimator
+    :param input_test_df: The input features of the testing data as a DataFrame.
+    :type input_test_df: pd.DataFrame
+    :param labels_test_df: The true labels of the testing data as a DataFrame.
+    :type labels_test_df: pd.DataFrame
+    :param plot_title: The title of the precision-recall curve plot. This is an optional
+     parameter.
+    :type plot_title: str, optional
+    :return: Base64 encoded image of the plot
+    :rtype: bytes"""
     input_test_df = input_test_df.set_index("Sample")
     label_encoder, y_test_encoded = encode_labels(
         input_test_df, labels_test_df, "Label"
