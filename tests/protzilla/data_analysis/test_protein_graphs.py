7f58407aeb361cfa95d574f2e200c644ec52a8f1
antonneubauer <antongneubauer@gmail.com>
2023-06-14 13:37:52 +0200

    cleanup tests

--- tests/protzilla/data_analysis/test_protein_graphs.py
+++ tests/protzilla/data_analysis/test_protein_graphs.py
@@ -453,14 +453,14 @@ def test_get_ref_seq_empty_seq():
     protein_path = Path(TEST_DATA_PATH, "proteins", "empty_seq.txt")
     error_msg = f"Could not find sequence for protein at path {protein_path}"
     with pytest.raises(ValueError, match=re.escape(error_msg)):
-        ref_seq, seq_len = _get_ref_seq(str(protein_path))
+        _get_ref_seq(str(protein_path))
 
 
 def test_get_ref_seq_no_seq_len():
     protein_path = Path(TEST_DATA_PATH, "proteins", "no_seq_len.txt")
     error_msg = f"Could not find lines with Sequence in {protein_path}"
     with pytest.raises(ValueError, match=re.escape(error_msg)):
-        ref_seq, seq_len = _get_ref_seq(str(protein_path))
+        _get_ref_seq(str(protein_path))
 
 
 def test_match_peptides_k5():
