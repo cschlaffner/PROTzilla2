39dcf859722936b6be2405e3493bd6d1e148c8e3
Fynn <fynn_kroeger@web.de>
2023-06-22 10:28:57 +0200

    fix tests for new interface

--- tests/protzilla/data_integration/test_database_integration.py
+++ tests/protzilla/data_integration/test_database_integration.py
@@ -29,19 +29,21 @@ def test_add_uniprot_data(mock_uniprot_query_dataframe, p_value_df):
             "Gene": ["MAPT", "ZXY;M99", None],
         }
     )
-    assert expected_df.equals(add_uniprot_data(p_value_df, "Gene")["results_df"])
+    assert expected_df.equals(
+        add_uniprot_data(p_value_df, "dummy", "Gene")["results_df"]
+    )
 
 
 @patch("protzilla.data_integration.database_query.uniprot_query_dataframe")
 def test_add_uniprot_links(mock_uniprot_query_dataframe, p_value_df):
-    links = add_uniprot_data(p_value_df, "Links")["results_df"]["Links"]
+    links = add_uniprot_data(p_value_df, "dummy", ["Links"])["results_df"]["Links"]
     assert all(all(x.startswith("https://") for x in link.split()) for link in links)
     assert not mock_uniprot_query_dataframe.called
 
 
 @patch("protzilla.data_integration.database_query.uniprot_query_dataframe")
 def test_add_uniprot_no_fileds(mock_uniprot_query_dataframe, p_value_df):
-    output = add_uniprot_data(p_value_df, [])
+    output = add_uniprot_data(p_value_df, "dummy", [])
     assert p_value_df.equals(output["results_df"])
     assert not mock_uniprot_query_dataframe.called
     assert "messages" in output
