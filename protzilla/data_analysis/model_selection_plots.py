53c61f1528059b283f2fd91230976c3a760d0489
Sara Grau <sara.grau-i-blade@student.hpi.uni-potsdam.de>
2023-06-27 16:19:55 +0200

    added nested cv to learning curve

--- protzilla/data_analysis/model_selection_plots.py
+++ protzilla/data_analysis/model_selection_plots.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
@@ -7,6 +8,7 @@ from sklearn.svm import SVC
 from protzilla.data_analysis.classification_helper import (
     perform_cross_validation,
     encode_labels,
+    perform_nested_cross_validation,
 )
 from protzilla.utilities import fig_to_base64
 from protzilla.utilities.transform_dfs import is_long_format, long_to_wide
@@ -30,8 +32,10 @@ def learning_curve_plot(
     cross_validation_strategy,
     scoring,
     random_state,
-    **cv_params
+    shuffle=True,
+    **cv_params,
 ):
+    # train_sizes = np.linspace(0.1, 1.0, 10)
     input_df_wide = long_to_wide(input_df) if is_long_format(input_df) else input_df
     input_df_wide.sort_values(by="Sample", inplace=True)
     labels_df = (
@@ -39,24 +43,56 @@ def learning_curve_plot(
         .set_index("Sample")
         .sort_values(by="Sample")
     )
+    common_indices = input_df_wide.index.intersection(labels_df.index)
+    labels_df = labels_df.loc[common_indices]
     encoding_mapping, labels_df = encode_labels(
         labels_df, labels_column, positive_label
     )
     clf = estimator_mapping[clf_str]
-    cv_callable = perform_cross_validation(
-        cross_validation_strategy, random_state_cv=random_state, **cv_params
-    )
 
-    display = LearningCurveDisplay.from_estimator(
-        clf,
-        input_df_wide,
-        labels_df["Encoded Label"],
-        train_sizes=train_sizes,
-        scoring=scoring,
-        cv=cv_callable,
-        random_state=random_state,
-        score_type="both",
-    )
+    if "Nested" in cross_validation_strategy:
+        if shuffle == "yes":
+            random_indices = np.random.RandomState(random_state).permutation(
+                input_df_wide.index
+            )
+            input_df_wide = input_df_wide.loc[random_indices]
+            labels_df = labels_df.loc[random_indices]
+        test_scores = list()
+        train_scores = list()
+        for size in train_sizes:
+            test_score, train_score = perform_nested_cross_validation(
+                input_df_wide.iloc[:size],
+                labels_df["Encoded Label"].iloc[:size],
+                clf,
+                scoring,
+                random_state=random_state,
+                **cv_params,
+            )
+            test_scores.append(test_score)
+            train_scores.append(train_score)
+            print(f"train_size{size}")
+
+        display = LearningCurveDisplay(
+            train_sizes=np.array(train_sizes),
+            train_scores=np.array(train_scores),
+            test_scores=np.array(test_scores),
+        )
+
+    else:
+        cv_callable = perform_cross_validation(
+            cross_validation_strategy, random_state_cv=random_state, **cv_params
+        )
+
+        display = LearningCurveDisplay.from_estimator(
+            clf,
+            input_df_wide,
+            labels_df["Encoded Label"],
+            train_sizes=train_sizes,
+            scoring=scoring,
+            cv=cv_callable,
+            random_state=random_state,
+            score_type="both",
+        )
 
     display.plot(
         score_type="both",
