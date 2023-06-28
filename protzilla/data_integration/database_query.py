d8644c15551fb14c396e0b37e74ca73f5985c239
Fynn <fynn_kroeger@web.de>
2023-06-28 16:46:57 +0200

    add group mapping, fix no internet

--- protzilla/data_integration/database_query.py
+++ protzilla/data_integration/database_query.py
@@ -1,10 +1,11 @@
 from xml.etree.ElementTree import Element, SubElement, tostring
-
+from collections import defaultdict
 import pandas
 import requests
 
 from protzilla.constants.logging import logger
 from protzilla.constants.paths import EXTERNAL_DATA_PATH
+from protzilla.utilities import clean_uniprot_id
 
 
 def biomart_query(queries, filter_name, attributes):
@@ -33,11 +34,14 @@ def biomart_query(queries, filter_name, attributes):
     )
     for attribute in attributes:
         SubElement(dataset, "Attribute", attrib={"name": attribute})
-    response = requests.post(
-        url="http://grch37.ensembl.org/biomart/martservice",
-        data={"query": tostring(root)},
-        stream=True,
-    )
+    try:
+        response = requests.post(
+            url="http://grch37.ensembl.org/biomart/martservice",
+            data={"query": tostring(root)},
+            stream=True,
+        )
+    except requests.ConnectionError:
+        return
     for line in response.iter_lines():
         yield line.decode("utf-8").split("\t")
 
@@ -74,7 +78,7 @@ def uniprot_databases():
 
 
 def uniprot_to_genes(uniprot_ids):
-    """Takes a list of uniprot IDs and maps them to genes"""
+    """Takes a list of uniprot IDs and maps them to genes. also returns IDs that could not not be mapped"""
     available_databases = uniprot_databases()
     out_dict = {}
     ids_to_search = uniprot_ids
@@ -106,13 +110,28 @@ def uniprot_to_genes(uniprot_ids):
         )
     )
     uniprot_id_to_hgnc_symbol = dict(set(map(tuple, biomart_results)))
+    # should not overwrite anything as ids_to_search not in out_dict yet
     out_dict.update(uniprot_id_to_hgnc_symbol)
     not_found = [id_ for id_ in ids_to_search if id_ not in uniprot_id_to_hgnc_symbol]
     return out_dict, not_found
 
 
-from time import time
-
-t = time()
-print(uniprot_to_genes(["P10636", "aa"]))
-print(time() - t)
+def uniprot_groups_to_genes(uniprot_groups):
+    proteins = set()
+    for group in uniprot_groups:
+        for protein in group.split(";"):
+            proteins.add(clean_uniprot_id(protein))
+    id_to_gene, not_found = uniprot_to_genes(list(proteins))
+    group_to_genes = {}
+    gene_to_groups = defaultdict(list)
+    filtered = []
+    for group in uniprot_groups:
+        clean = set(clean_uniprot_id(protein) for protein in group.split(";"))
+        results = list(filter(lambda r: bool(r), map(id_to_gene.get, clean)))
+        if not results:
+            filtered.append(group)
+        else:
+            group_to_genes[group] = results
+            for result in results:
+                gene_to_groups[result].append(group)
+    return group_to_genes, dict(gene_to_groups), filtered
