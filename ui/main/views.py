86e8627a5bce923ef46856e16dd3a68c21a15495
Fynn <fynn_kroeger@web.de>
2023-07-03 20:52:09 +0200

    add functionality to backend

--- ui/main/views.py
+++ ui/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import redirect, render
 from protzilla.data_integration.database_query import uniprot_databases
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import pandas
+from django.contrib import messages
+from protzilla.constants.paths import EXTERNAL_DATA_PATH
 
 
 def index(request):
@@ -12,3 +17,23 @@ def databases(request):
         "databases.html",
         context=dict(uniprot_databases=uniprot_databases()),
     )
+
+
+def database_upload(request):
+    # add option to copy and not verify the file?
+    chosen_name = request.POST["name"]
+    path = dict(request.FILES)["new-db"][0].temporary_file_path()
+    try:
+        dataframe = pandas.read_csv(path, sep="\t")
+    except UnicodeDecodeError:
+        messages.add_message(request, 40, "File could not be decoded.", "alert-danger")
+        return HttpResponseRedirect(reverse("databases"))
+    if "Entry" not in dataframe.columns:
+        messages.add_message(
+            request, 40, "Required 'Entry' column not found.", "alert-danger"
+        )
+        return HttpResponseRedirect(reverse("databases"))
+    dataframe.to_csv(
+        EXTERNAL_DATA_PATH / "uniprot" / f"{chosen_name}.tsv", sep="\t", index=False
+    )
+    return HttpResponseRedirect(reverse("databases"))
