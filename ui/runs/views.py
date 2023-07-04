608a71cf912d7526b919225e995c5e5a377d14e1
antonneubauer <antongneubauer@gmail.com>
2023-07-04 21:04:00 +0200

    more filtered blocks

--- ui/runs/views.py
+++ ui/runs/views.py
@@ -587,6 +587,7 @@ def protein_graph(request, run_name, index: int):
             "peptide_matches": peptide_matches,
             "peptide_mismatches": peptide_mismatches,
             "protein_id": protein_id,
+            "filtered_blocks": outputs.get("filtered_blocks", []),
             "run_name": run_name,
             "used_memory": get_memory_usage(),
         },
