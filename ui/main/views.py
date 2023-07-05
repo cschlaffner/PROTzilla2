6a60af6cbcc7a6e2dd3004cc7910f7eb2a6de05e
antonneubauer <antongneubauer@gmail.com>
2023-07-05 12:08:54 +0200

    error handling for file types

--- ui/main/views.py
+++ ui/main/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import redirect, render
-from protzilla.data_integration.database_query import uniprot_databases, uniprot_columns
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 import pandas
 from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
 from protzilla.constants.paths import EXTERNAL_DATA_PATH
+from protzilla.data_integration.database_query import uniprot_columns, uniprot_databases
 
 
 def index(request):
@@ -25,16 +26,32 @@ def database_upload(request):
     # add option to copy and not verify the file?
     chosen_name = request.POST["name"]
     path = dict(request.FILES)["new-db"][0].temporary_file_path()
+    if not path.endswith(".tsv") or path.endswith(".csv"):
+        messages.add_message(
+            request,
+            messages.ERROR,
+            "File must be a tab-separated file (.tsv, .csv).",
+            "alert-danger",
+        )
+        return HttpResponseRedirect(reverse("databases"))
     try:
         dataframe = pandas.read_csv(path, sep="\t")
     except UnicodeDecodeError:
-        messages.add_message(request, 40, "File could not be decoded.", "alert-danger")
+        messages.add_message(
+            request, messages.ERROR, "File could not be decoded.", "alert-danger"
+        )
         return HttpResponseRedirect(reverse("databases"))
     if "Entry" not in dataframe.columns:
         messages.add_message(
-            request, 40, "Required 'Entry' column not found.", "alert-danger"
+            request,
+            messages.ERROR,
+            "Required 'Entry' column not found.",
+            "alert-danger",
         )
         return HttpResponseRedirect(reverse("databases"))
+    if not (EXTERNAL_DATA_PATH / "uniprot").exists():
+        (EXTERNAL_DATA_PATH / "uniprot").mkdir(parents=True, exist_ok=True)
+
     dataframe.to_csv(
         EXTERNAL_DATA_PATH / "uniprot" / f"{chosen_name}.tsv", sep="\t", index=False
     )
