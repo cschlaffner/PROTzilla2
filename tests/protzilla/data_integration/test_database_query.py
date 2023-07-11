96026cac01aab25fd6cf09a9ae880bcb663d1a65
Fynn <fynn_kroeger@web.de>
2023-07-11 15:04:46 +0200

    fix database query tests

--- tests/protzilla/data_integration/test_database_query.py
+++ tests/protzilla/data_integration/test_database_query.py
@@ -12,7 +12,7 @@ from protzilla.data_integration.database_query import (
 def test_uniprot_groups_to_genes(mock_uniprot_to_genes):
     mock_uniprot_to_genes.return_value = dict(a="ABS", b="HKL", c="NNF"), ["d"]
     gene_to_groups, group_to_genes, filtered = uniprot_groups_to_genes(
-        ["a;b;c", "a-1;b-1_4", "c;d", "d-4;d-8"]
+        ["a;b;c", "a-1;b-1_4", "c;d", "d-4;d-8"], ["db_name"]
     )
     assert filtered == ["d-4;d-8"]
     expected_gene_map = {
@@ -34,48 +34,40 @@ def test_uniprot_groups_to_genes(mock_uniprot_to_genes):
         assert set(group_to_genes[key]) == set(expected_group_map[key])
 
 
-@patch("protzilla.data_integration.database_query.uniprot_databases")
 @patch("protzilla.data_integration.database_query.biomart_query")
-def test_uniprot_to_genes_biomart(mock_biomart, mock_databases):
+def test_uniprot_to_genes_biomart(mock_biomart):
     # no databases found, only biomart will be used to map
-    mock_databases.return_value = []
     mock_biomart.return_value = [["a", "ABS"], ["b", "HKL"], ["c", "NNF"]]
-    mapping, not_found = uniprot_to_genes(list("abcd"))
+    mapping, not_found = uniprot_to_genes(list("abcd"), [])
     assert mapping == dict(a="ABS", b="HKL", c="NNF")
     assert not_found == ["d"]
 
 
 @patch("protzilla.data_integration.database_query.uniprot_query_dataframe")
 @patch("protzilla.data_integration.database_query.uniprot_columns")
-@patch("protzilla.data_integration.database_query.uniprot_databases")
 @patch("protzilla.data_integration.database_query.biomart_query")
-def test_uniprot_to_genes_database(
-    mock_biomart, mock_databases, mock_columns, mock_query
-):
+def test_uniprot_to_genes_database(mock_biomart, mock_columns, mock_query):
     # database found, biomart will not find anything
     mock_query.return_value = pd.DataFrame(
         {"Gene Names": ["ABS", "HKL GST", "NNF"]}, index=["a", "b", "c"]
     )
     mock_columns.return_value = ["Gene Names"]
-    mock_databases.return_value = ["hi"]
     mock_biomart.return_value = []
-    mapping, not_found = uniprot_to_genes(list("abcd"))
+    mapping, not_found = uniprot_to_genes(list("abcd"), ["db_name"])
     assert mapping == dict(a="ABS", b="HKL", c="NNF")
     assert not_found == ["d"]
 
 
 @patch("protzilla.data_integration.database_query.uniprot_query_dataframe")
 @patch("protzilla.data_integration.database_query.uniprot_columns")
-@patch("protzilla.data_integration.database_query.uniprot_databases")
 @patch("protzilla.data_integration.database_query.biomart_query")
-def test_uniprot_to_genes_both(mock_biomart, mock_databases, mock_columns, mock_query):
+def test_uniprot_to_genes_both(mock_biomart, mock_columns, mock_query):
     # both database and biomart used for mapping
     mock_query.return_value = pd.DataFrame(
         {"Gene Names": ["ABS", "HKL GST"]}, index=["a", "b"]
     )
     mock_columns.return_value = ["Gene Names"]
-    mock_databases.return_value = ["hi"]
     mock_biomart.return_value = [["c", "NNF"]]
-    mapping, not_found = uniprot_to_genes(list("abcd"))
+    mapping, not_found = uniprot_to_genes(list("abcd"), ["db_name"])
     assert mapping == dict(a="ABS", b="HKL", c="NNF")
     assert not_found == ["d"]
