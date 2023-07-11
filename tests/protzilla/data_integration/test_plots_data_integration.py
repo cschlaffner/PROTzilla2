86552c8005bd06ab8f01426228231363ecdc4da6
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2023-07-11 11:46:52 +0200

    Cleanup enrichment analysis (#270)
    * renaming
    
    * further improvements
    
    * update parameters
    
    * add figsize param
    
    * Update enrichment_analysis.py
    
    * update param
    
    * Move error handling up
    
    * fix biomart query
    
    * fix order of gene_sets if

--- tests/protzilla/data_integration/test_plots_data_integration.py
+++ tests/protzilla/data_integration/test_plots_data_integration.py
@@ -5,8 +5,8 @@ import pytest
 
 from protzilla.constants.paths import PROJECT_PATH
 from protzilla.data_integration.di_plots import (
-    go_enrichment_bar_plot,
-    go_enrichment_dot_plot,
+    GO_enrichment_bar_plot,
+    GO_enrichment_dot_plot,
     gsea_dot_plot,
     gsea_enrichment_plot,
 )
@@ -20,22 +20,22 @@ def data_folder_tests():
 def test_enrichment_bar_plot_restring(show_figures, helpers):
     test_data_folder = f"{PROJECT_PATH}/tests/test_data/enrichment_data"
     result = pd.read_csv(f"{test_data_folder}/merged_KEGG_process.csv", header=0)
-    bar_base64 = go_enrichment_bar_plot(
+    bar_base64 = GO_enrichment_bar_plot(
         input_df=result,
-        categories=["KEGG", "Process"],
         top_terms=10,
         cutoff=0.05,
         value="fdr",
+        gene_sets=["KEGG", "Process"],
     )
     if show_figures:
         helpers.open_graph_from_base64(bar_base64[0])
 
-    bar_base64 = go_enrichment_bar_plot(
+    bar_base64 = GO_enrichment_bar_plot(
         input_df=result,
-        categories=["KEGG", "Process"],
         top_terms=10,
         cutoff=0.05,
         value="p_value",
+        gene_sets=["KEGG", "Process"],
     )
     if show_figures:
         helpers.open_graph_from_base64(bar_base64[0])
@@ -45,12 +45,12 @@ def test_enrichment_bar_plot(show_figures, helpers, data_folder_tests):
     enrichment_df = pd.read_csv(
         data_folder_tests / "Reactome_enrichment_enrichr.csv", sep="\t"
     )
-    bar_base64 = go_enrichment_bar_plot(
+    bar_base64 = GO_enrichment_bar_plot(
         input_df=enrichment_df,
-        categories=["Reactome_2013"],
         top_terms=10,
         cutoff=0.05,
         value="p_value",
+        gene_sets=["Reactome_2013"],
     )
     if show_figures:
         helpers.open_graph_from_base64(bar_base64[0])
@@ -60,12 +60,12 @@ def test_enrichment_bar_plot_wrong_value(data_folder_tests):
     enrichment_df = pd.read_csv(
         data_folder_tests / "Reactome_enrichment_enrichr.csv", sep="\t"
     )
-    current_out = go_enrichment_bar_plot(
+    current_out = GO_enrichment_bar_plot(
         input_df=enrichment_df,
-        categories=["Reactome_2013"],
         top_terms=10,
         cutoff=0.05,
         value="fdr",
+        gene_sets=["Reactome_2013"],
     )[0]
     assert "messages" in current_out
     assert "FDR is not available" in current_out["messages"][0]["msg"]
@@ -73,12 +73,12 @@ def test_enrichment_bar_plot_wrong_value(data_folder_tests):
 
 def test_enrichment_bar_plot_empty_df():
     empty_df = pd.DataFrame()
-    current_out = go_enrichment_bar_plot(
+    current_out = GO_enrichment_bar_plot(
         input_df=empty_df,
-        categories=["Reactome_2013"],
         top_terms=10,
         cutoff=0.05,
         value="p_value",
+        gene_sets=["Reactome_2013"],
     )[0]
     assert "messages" in current_out
     assert "No data to plot" in current_out["messages"][0]["msg"]
@@ -88,12 +88,8 @@ def test_enrichment_bar_plot_no_category(data_folder_tests):
     enrichment_df = pd.read_csv(
         data_folder_tests / "Reactome_enrichment_enrichr.csv", sep="\t"
     )
-    current_out = go_enrichment_bar_plot(
-        input_df=enrichment_df,
-        categories=[],
-        top_terms=10,
-        cutoff=0.05,
-        value="p_value",
+    current_out = GO_enrichment_bar_plot(
+        input_df=enrichment_df, top_terms=10, cutoff=0.05, value="p_value", gene_sets=[]
     )[0]
     assert "messages" in current_out
     assert "Please select at least one category" in current_out["messages"][0]["msg"]
@@ -101,12 +97,12 @@ def test_enrichment_bar_plot_no_category(data_folder_tests):
 
 def test_enrichment_bar_plot_wrong_df():
     enrichment_df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
-    current_out = go_enrichment_bar_plot(
+    current_out = GO_enrichment_bar_plot(
         input_df=enrichment_df,
-        categories=["KEGG"],
         top_terms=10,
         cutoff=0.05,
         value="p_value",
+        gene_sets=["KEGG"],
     )[0]
     assert "messages" in current_out
     assert (
@@ -117,12 +113,12 @@ def test_enrichment_bar_plot_wrong_df():
 
 def test_enrichment_bar_plot_cutoff(data_folder_tests):
     result = pd.read_csv(data_folder_tests / "merged_KEGG_process.csv", header=0)
-    current_out = go_enrichment_bar_plot(
+    current_out = GO_enrichment_bar_plot(
         input_df=result,
-        categories=["KEGG", "Process"],
         top_terms=10,
         cutoff=0,
         value="fdr",
+        gene_sets=["KEGG", "Process"],
     )[0]
 
     assert "messages" in current_out
@@ -131,32 +127,32 @@ def test_enrichment_bar_plot_cutoff(data_folder_tests):
     enrichment_df = pd.read_csv(
         data_folder_tests / "Reactome_enrichment_enrichr.csv", sep="\t"
     )
-    current_out = go_enrichment_bar_plot(
+    current_out = GO_enrichment_bar_plot(
         input_df=enrichment_df,
-        categories=["Reactome_2013"],
         top_terms=10,
         cutoff=0,
         value="p_value",
+        gene_sets=["Reactome_2013"],
     )[0]
     assert "messages" in current_out
     assert "No data to plot when applying cutoff" in current_out["messages"][0]["msg"]
 
 
 @pytest.mark.parametrize("x_axis_type", ["Gene Sets", "Combined Score"])
-def test_go_enrichment_dot_plot(helpers, show_figures, x_axis_type, data_folder_tests):
+def test_GO_enrichment_dot_plot(helpers, show_figures, x_axis_type, data_folder_tests):
     enrichment_df = pd.read_csv(
         data_folder_tests / "Reactome_enrichment_enrichr.csv", header=0, sep="\t"
     )
-    dot_base64 = go_enrichment_dot_plot(
+    dot_base64 = GO_enrichment_dot_plot(
         input_df=enrichment_df,
-        categories=["Reactome_2013"],
-        x_axis_type=x_axis_type,
         top_terms=5,
         cutoff=0.05,
-        dot_size=40,
+        gene_sets=["Reactome_2013"],
+        x_axis_type=x_axis_type,
         title="Reactome Enrichment Test",
         rotate_x_labels=False,
         show_ring=False,
+        dot_size=40,
     )[0]
     if show_figures:
         helpers.open_graph_from_base64(dot_base64["plot_base64"])
@@ -164,11 +160,8 @@ def test_go_enrichment_dot_plot(helpers, show_figures, x_axis_type, data_folder_
 
 def test_enrichment_dot_plot_wrong_df(data_folder_tests):
     result = pd.read_csv(data_folder_tests / "merged_KEGG_process.csv", header=0)
-    current_out = go_enrichment_dot_plot(
-        input_df=result,
-        categories=["KEGG", "Process"],
-        top_terms=10,
-        cutoff=0.05,
+    current_out = GO_enrichment_dot_plot(
+        input_df=result, top_terms=10, cutoff=0.05, gene_sets=["KEGG", "Process"]
     )[0]
 
     assert "messages" in current_out
