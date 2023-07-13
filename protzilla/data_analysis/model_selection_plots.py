7861dd7822d887fd54724bfadafb626570c34dea
Sara Grau <sara.grau-i-blade@student.hpi.uni-potsdam.de>
2023-07-13 20:57:51 +0200

    updated how flattenign point of a curve is found

--- protzilla/data_analysis/model_selection_plots.py
+++ protzilla/data_analysis/model_selection_plots.py
@@ -1,13 +1,14 @@
 import numpy as np
 from kneed import KneeLocator
 from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
 from sklearn.model_selection import LearningCurveDisplay
 
 from protzilla.utilities import fig_to_base64, remove_underscore_and_capitalize
 
 
 def learning_curve_plot(
-    train_sizes, train_scores, test_scores, score_name, minimum_viable_sample_size
+    train_sizes, train_scores, test_scores, score_name, initial_coef_sigmoid
 ):
     # learning curve with training and validation score
     display = LearningCurveDisplay(
@@ -26,28 +27,36 @@ def learning_curve_plot(
     for text, label in zip(legend.get_texts(), legend_labels):
         text.set_text(label)
 
-    # learning curve for test scores with elbow
-    display_elbow = LearningCurveDisplay(
-        train_sizes=train_sizes,
-        train_scores=np.array(train_scores),
-        test_scores=np.array(test_scores),
-        score_name=score_name,
+    # find flattening point in learning curve
+    def sigmoid(x, a, b, c):
+        return a / (1 + np.exp(-b * (x - c)))
+
+    test_scores_mean = np.array(test_scores).mean(axis=1)
+    params, _ = curve_fit(
+        sigmoid, train_sizes, test_scores_mean, p0=initial_coef_sigmoid
     )
-    display_elbow.plot(
-        score_type="test",
-        std_display_style=None,
-        line_kw={"marker": "o", "color": "#ff7f0a"},
-    )
-    display_elbow.ax_.axvline(
-        minimum_viable_sample_size,
+    x_fit = np.linspace(min(train_sizes), max(train_sizes), 100)
+    curve_values = sigmoid(x_fit, *params)
+    diff = np.diff(curve_values)
+    flattening_point_idx = np.argmax(diff < np.mean(diff))
+    flattening_point = x_fit[flattening_point_idx]
+
+    plt.figure()
+    plt.plot(train_sizes, test_scores_mean, "o-", label=f"Test {score_name}")
+    plt.plot(x_fit, curve_values, "r-", label="Fitted Curve")
+    plt.axvline(
+        flattening_point,
         ls="--",
         color="gray",
         label="Minimum Viable Sample Size",
     )
+    plt.xlabel("Training Set Size")
+    plt.ylabel(score_name)
+    plt.ylim(0.4, 1.0)
+    plt.legend()
+    display_flat = plt.gcf()
 
-    plt.legend([f"Test {score_name}", "Minimum Viable Sample Size"])
-
-    return [fig_to_base64(display.figure_), fig_to_base64(display_elbow.figure_)]
+    return [fig_to_base64(display.figure_), fig_to_base64(display_flat)]
 
 
 def elbow_method_n_clusters(
