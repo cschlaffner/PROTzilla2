f06b1ebd10acddca2149d4572a1e24816b6e22f3
Fynn <fynn_kroeger@web.de>
2023-07-25 22:30:21 +0200

    use paged, fix path, better progress

--- protzilla/data_integration/database_download.py
+++ protzilla/data_integration/database_download.py
@@ -6,7 +6,8 @@ from requests.adapters import HTTPAdapter, Retry
 from tqdm import tqdm
 
 # cannot be imported form constats as package cannot be found
-DATABASES_PATH = Path(__file__).parent.parent.parent / "user_data" / "external_data"
+external_data_path = Path(__file__).parent.parent.parent / "user_data" / "external_data"
+uniprot_db_path = external_data_path / "uniprot"
 
 
 def get_next_link(headers):
@@ -27,7 +28,7 @@ def get_batch(batch_url, session):
         batch_url = get_next_link(response.headers)
 
 
-def download_uniprot_paged():
+def download_uniprot_paged(name):
     """downloads basic info on all human proteins from the uniprot paged rest api.
     this will take very long due to limitations in the api, therefore stream should be used.
     code taken from https://www.uniprot.org/help/api_queries including get_next_link and get_batch
@@ -39,7 +40,7 @@ def download_uniprot_paged():
 
     url = "https://rest.uniprot.org/uniprotkb/search?fields=accession,id,protein_name,gene_names,organism_name,length&format=tsv&query=%28organism_id:9606%29%20AND%20%28reviewed%3Atrue%29&size=500"
     progress = 0
-    with open(DATABASES_PATH / "uniprot.tsv", "w") as f:
+    with open(uniprot_db_path / f"{name}.tsv", "w") as f:
         for batch, total in get_batch(url, session):
             lines = batch.text.splitlines()
             if not progress:
@@ -47,10 +48,11 @@ def download_uniprot_paged():
             for line in lines[1:]:
                 print(line, file=f)
             progress += len(lines[1:])
-            print(f"{progress} / {total}")
+            print(f"\rprogress: {progress} / {total}", end="")
+        print()
 
 
-def download_uniprot_stream():
+def download_uniprot_stream(name):
     """downloads basic info on all human proteins from the streamed uniprot rest api.
     can fail due to unstable internet connection or problems with the api."""
     with requests.get(
@@ -64,27 +66,26 @@ def download_uniprot_stream():
         stream=True,
     ) as r:
         r.raise_for_status()
-        with open(DATABASES_PATH / "uniprot.tsv", "wb") as f:
+        with open(uniprot_db_path / f"{name}.tsv", "wb") as f:
             for chunk in tqdm(r.iter_content(chunk_size=8192)):
                 f.write(chunk)
 
 
 if __name__ == "__main__":
-    DATABASES_PATH.mkdir(exist_ok=True)
-    if not (DATABASES_PATH / "uniprot.tsv").exists():
+    uniprot_db_path.mkdir(exist_ok=True, parents=True)
+    databases = [path for path in uniprot_db_path.iterdir() if path.suffix == ".tsv"]
+    if not databases:
         print("no Uniprot database found, starting to download")
         print("this will take 1-5 minutes")
         try:
-            download_uniprot_stream()
+            download_uniprot_paged("human_reviewed")
         except Exception as e:
             print(e)
             print(
-                "\n\nfailed to download Uniprot, you can download it from "
-                "\nhttps://www.uniprot.org/uniprotkb?query=*"
-                "\nor from the protzilla release on github. for more info go to "
-                "\nhttps://github.com/antonneubauer/PROTzilla2/wiki/Databases"
+                "\n\nfailed to download from UniProt, you can download it from "
+                "\nhttps://www.uniprot.org/uniprotkb"
+                "\nor from the protzilla release on github. "
+                "\nfor more info go to the protzilla documentation on downloading UniProt"
             )
         else:
             print("done downloading")
-    else:
-        print("Uniprot already downloaded")
