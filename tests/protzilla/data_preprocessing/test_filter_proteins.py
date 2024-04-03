9b983dd9f93668765166eb942172b81d1e5a671f
janni.roebbecke <janni.roebbecke@student.hpi.uni-potsdam.de>
2024-04-03 15:21:53 +0200

    tests for filter_proteins with peptide file

--- tests/protzilla/data_preprocessing/test_filter_proteins.py
+++ tests/protzilla/data_preprocessing/test_filter_proteins.py
@@ -70,10 +70,44 @@ def filter_proteins_by_samples_missing_df():
     return df
 
 
+@pytest.fixture
+def filter_peptides_df():
+    df = pd.DataFrame(
+        (
+            ["Sample1", "Protein1", "Gene1", "Peptide1", 1],
+            ["Sample1", "Protein2", "Gene1", "Peptide2", 1],
+            ["Sample1", "Protein2", "Gene1", "Peptide3", 1],
+            ["Sample1", "Protein2", "Gene1", "Peptide4", 1],
+            ["Sample1", "Protein3", "Gene1", "Peptide5", 1],
+            ["Sample1", "Protein3", "Gene1", "Peptide6", 1],
+            ["Sample1", "Protein3", "Gene1", "Peptide7", 1],
+            ["Sample1", "Protein4", "Gene1", "Peptide8", 1],
+            ["Sample1", "Protein5", "Gene1", "Peptide9", 1],
+            ["Sample2", "Protein1", "Gene1", "Peptide10", 1],
+            ["Sample2", "Protein2", "Gene1", "Peptide11", 1],
+            ["Sample2", "Protein3", "Gene1", "Peptide12", 1],
+            ["Sample2", "Protein4", "Gene1", "Peptide13", 1],
+            ["Sample2", "Protein5", "Gene1", "Peptide14", 1],
+            ["Sample3", "Protein1", "Gene1", "Peptide15", 1],
+            ["Sample3", "Protein2", "Gene1", "Peptide16", 1],
+            ["Sample3", "Protein3", "Gene1", "Peptide17", 1],
+            ["Sample3", "Protein4", "Gene1", "Peptide18", 1],
+            ["Sample3", "Protein5", "Gene1", "Peptide19", 1],
+            ["Sample4", "Protein1", "Gene1", "Peptide20", 1],
+            ["Sample4", "Protein2", "Gene1", "Peptide21", 1],
+            ["Sample4", "Protein3", "Gene1", "Peptide22", 1],
+            ["Sample4", "Protein4", "Gene1", "Peptide23", 1],
+        ),
+        columns=["Sample", "Protein ID", "Gene", "Peptide", "Intensity"],
+    )
+
+    return df
+
+
 def test_filter_proteins_by_missing_samples(
     filter_proteins_by_samples_missing_df, show_figures
 ):
-    result_df, method_output = by_samples_missing(
+    result_df, _, method_output = by_samples_missing(
         filter_proteins_by_samples_missing_df, percentage=1.0
     )
 
@@ -89,16 +123,37 @@ def test_filter_proteins_by_missing_samples(
         "Protein5",
     ]
 
-    result_df, method_output = by_samples_missing(
+    result_df, _, method_output = by_samples_missing(
         filter_proteins_by_samples_missing_df, percentage=0.5
     )
     method_output["filtered_proteins"]
 
     assert method_output["filtered_proteins"] == ["Protein4", "Protein5"]
 
-    result_df, method_output = by_samples_missing(
+    result_df, _, method_output = by_samples_missing(
         filter_proteins_by_samples_missing_df, percentage=0.0
     )
     method_output["filtered_proteins"]
 
     assert method_output["filtered_proteins"] == []
+
+
+def test_proteins_by_missing_samples_with_peptides(
+        filter_proteins_df, filter_peptides_df, show_figures
+):
+    result_df, result_peptide_df, method_output = by_samples_missing(
+        filter_proteins_df, percentage=1.0
+    )
+
+    assert result_peptide_df is None
+
+    result_df, result_peptide_df, method_output = by_samples_missing(
+        filter_proteins_df, filter_peptides_df, percentage=1.0
+    )
+
+    assert result_peptide_df is not None
+    assert (
+        not result_peptide_df["Protein ID"]
+        .isin(method_output["filtered_proteins"])
+        .any()
+    )
