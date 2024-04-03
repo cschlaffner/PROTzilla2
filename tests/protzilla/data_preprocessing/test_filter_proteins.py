ba0d58ea04cf5477e59b8370865914fbe099c723
janni.roebbecke <janni.roebbecke@student.hpi.uni-potsdam.de>
2024-04-03 15:43:32 +0200

    tests for filter_proteins with peptide file, better mock dataframe

--- tests/protzilla/data_preprocessing/test_filter_proteins.py
+++ tests/protzilla/data_preprocessing/test_filter_proteins.py
@@ -71,34 +71,34 @@ def filter_proteins_by_samples_missing_df():
 
 
 @pytest.fixture
-def filter_peptides_df():
+def peptides_df():
     df = pd.DataFrame(
         (
-            ["Sample1", "Protein1", "Gene1", "Peptide1", 1],
-            ["Sample1", "Protein2", "Gene1", "Peptide2", 1],
-            ["Sample1", "Protein2", "Gene1", "Peptide3", 1],
-            ["Sample1", "Protein2", "Gene1", "Peptide4", 1],
-            ["Sample1", "Protein3", "Gene1", "Peptide5", 1],
-            ["Sample1", "Protein3", "Gene1", "Peptide6", 1],
-            ["Sample1", "Protein3", "Gene1", "Peptide7", 1],
-            ["Sample1", "Protein4", "Gene1", "Peptide8", 1],
-            ["Sample1", "Protein5", "Gene1", "Peptide9", 1],
-            ["Sample2", "Protein1", "Gene1", "Peptide10", 1],
-            ["Sample2", "Protein2", "Gene1", "Peptide11", 1],
-            ["Sample2", "Protein3", "Gene1", "Peptide12", 1],
-            ["Sample2", "Protein4", "Gene1", "Peptide13", 1],
-            ["Sample2", "Protein5", "Gene1", "Peptide14", 1],
-            ["Sample3", "Protein1", "Gene1", "Peptide15", 1],
-            ["Sample3", "Protein2", "Gene1", "Peptide16", 1],
-            ["Sample3", "Protein3", "Gene1", "Peptide17", 1],
-            ["Sample3", "Protein4", "Gene1", "Peptide18", 1],
-            ["Sample3", "Protein5", "Gene1", "Peptide19", 1],
-            ["Sample4", "Protein1", "Gene1", "Peptide20", 1],
-            ["Sample4", "Protein2", "Gene1", "Peptide21", 1],
-            ["Sample4", "Protein3", "Gene1", "Peptide22", 1],
-            ["Sample4", "Protein4", "Gene1", "Peptide23", 1],
+            ["Sample1", "Protein1", "SEQA", 1000000, 0.00001],
+            ["Sample1", "Protein2", "SEQB", 2000000, 0.00002],
+            ["Sample1", "Protein2", "SEQC", 3000000, 0.00003],
+            ["Sample1", "Protein2", "SEQD", 4000000, 0.00004],
+            ["Sample1", "Protein3", "SEQE", 5000000, 0.00005],
+            ["Sample1", "Protein3", "SEQF", 6000000, 0.00006],
+            ["Sample1", "Protein3", "SEQG", 7000000, 0.00007],
+            ["Sample1", "Protein4", "SEQH", 8000000, 0.00008],
+            ["Sample1", "Protein5", "SEQI", 9000000, 0.00009],
+            ["Sample2", "Protein1", "SEQJ", 10000000, 0.0001],
+            ["Sample2", "Protein2", "SEQK", 11000000, 0.00011],
+            ["Sample2", "Protein3", "SEQL", 12000000, 0.00012],
+            ["Sample2", "Protein4", "SEQM", 13000000, 0.00013],
+            ["Sample2", "Protein5", "SEQN", 14000000, 0.00014],
+            ["Sample3", "Protein1", "SEQO", 15000000, 0.00015],
+            ["Sample3", "Protein2", "SEQP", 16000000, 0.00016],
+            ["Sample3", "Protein3", "SEQQ", 17000000, 0.00017],
+            ["Sample3", "Protein4", "SEQR", 18000000, 0.00018],
+            ["Sample3", "Protein5", "SEQS", 19000000, 0.00019],
+            ["Sample4", "Protein1", "SEQT", 20000000, 0.0002],
+            ["Sample4", "Protein2", "SEQU", 21000000, 0.00021],
+            ["Sample4", "Protein3", "SEQV", 22000000, 0.00022],
+            ["Sample4", "Protein4", "SEQW", 23000000, 0.00023],
         ),
-        columns=["Sample", "Protein ID", "Gene", "Peptide", "Intensity"],
+        columns=["Sample", "Protein ID", "Sequence", "Intensity", "PEP"],
     )
 
     return df
@@ -139,7 +139,7 @@ def test_filter_proteins_by_missing_samples(
 
 
 def test_proteins_by_missing_samples_with_peptides(
-        filter_proteins_df, filter_peptides_df, show_figures
+    filter_proteins_df, peptides_df, show_figures
 ):
     result_df, result_peptide_df, method_output = by_samples_missing(
         filter_proteins_df, percentage=1.0
@@ -148,7 +148,7 @@ def test_proteins_by_missing_samples_with_peptides(
     assert result_peptide_df is None
 
     result_df, result_peptide_df, method_output = by_samples_missing(
-        filter_proteins_df, filter_peptides_df, percentage=1.0
+        filter_proteins_df, peptides_df, percentage=1.0
     )
 
     assert result_peptide_df is not None
