d3297093c857ff241f08fc6b6440cce27494b74a
henning <henning.gaertner@student.hpi.de>
2024-04-10 16:09:59 +0200

    new mapping class

--- ui/runs_v2/form_mapping.py
+++ ui/runs_v2/form_mapping.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest
 
 from protzilla.run import Run
+from protzilla.steps import MaxQuantImport, MetadataImport
 
 from .forms.base import MethodForm
 from .forms.importing import MaxQuantImportForm, MetadataImportForm
@@ -11,8 +12,10 @@ form_map = {
 }
 
 
-def get_empty_form_by_method(method: str, run: Run) -> MethodForm:
-    form_class = form_map.get(method)
+# TODO port to new below in file
+def get_empty_form_by_method(step, run: Run) -> MethodForm:
+    map = Mappings()
+    form_class = map.get_form_class_by_step(step)
     if form_class:
         return form_class(run=run)
     else:
@@ -20,9 +23,58 @@ def get_empty_form_by_method(method: str, run: Run) -> MethodForm:
 
 
 def get_filled_form_by_request(request: HttpRequest, run: Run) -> MethodForm:
+    map = Mappings()
     method = request.POST["chosen_method"]
-    form_class = form_map.get(method)
+    form_class = map.get_form_class_by_step(method)
     if form_class:
         return form_class(run=run, data=request.POST, files=request.FILES)
     else:
         raise ValueError(f"No form has been provided for {method} method.")
+
+
+class Mappings:
+    def __init__(self):
+        self.forward_mapping = {
+            MaxQuantImport: MaxQuantImportForm,
+            MetadataImport: MetadataImportForm,
+        }
+        self.reverse_mapping = {v: k for k, v in self.forward_mapping.items()}
+
+    def generate_hierarchical_dict():
+        # Initialize an empty dictionary
+        hierarchical_dict = {}
+
+        # List of all Step subclasses
+        step_classes = self.forward_mapping.keys()
+
+        # Iterate over each Step subclass
+        for step_class in step_classes:
+            # Create a nested dictionary with keys being the section, step, and method
+            # and the value being the class itself
+            if step_class.section not in hierarchical_dict:
+                hierarchical_dict[step_class.section] = {}
+            if step_class.step not in hierarchical_dict[step_class.section]:
+                hierarchical_dict[step_class.section][step_class.step] = {}
+            hierarchical_dict[step_class.section][step_class.step][
+                step_class.method
+            ] = step_class
+
+        return hierarchical_dict
+
+    def get_form_class_by_step(self, step):
+        form_class = self.forward_mapping.get(type(step))
+        if form_class:
+            return form_class
+        else:
+            raise ValueError(
+                f"No form has been provided for {type(step).__name__} step."
+            )
+
+    def get_step_class_by_form(self, form):
+        step_class = self.reverse_mapping.get(type(form))
+        if step_class:
+            return step_class
+        else:
+            raise ValueError(
+                f"No step has been provided for {type(form).__name__} form."
+            )
