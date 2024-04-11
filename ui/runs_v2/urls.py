cec449e07de83b6a51e745406203384dc4a6b710
henning <henning.gaertner@student.hpi.de>
2024-04-11 11:59:38 +0200

    Add back, next_ and tables to the urls

--- ui/runs_v2/urls.py
+++ ui/runs_v2/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("create", views.create, name="create"),
     path("continue", views.continue_, name="continue"),
     path("detail/<str:run_name>", views.detail, name="detail"),
+    path("<str:run_name>/tables/<int:index>", views.tables, name="tables_nokey"),
+    path("<str:run_name>/next", views.next_, name="next"),
+    path("<str:run_name>/back", views.back, name="back"),
 ]
