05291801ad0b9fbac5ccf452d33de057ff796a65
janni.roebbecke <janni.roebbecke@student.hpi.uni-potsdam.de>
2024-04-12 18:51:38 +0200

    make calculating preprocessing steps possible
    Co-authored-by: janni.roebbecke <janni.roebbecke@student.hpi.uni-potsdam.de>

--- ui/runs_v2/forms/data_preprocessing.py
+++ ui/runs_v2/forms/data_preprocessing.py
@@ -8,4 +8,7 @@ class ImputationMinPerProteinForm(MethodForm):
     shrinking_value = CustomNumberInput(label="Shrinking value")
 
     def submit(self, run: Run):
+        self.cleaned_data["shrinking_value"] = float(
+            self.cleaned_data["shrinking_value"]
+        )
         run.step_calculate(self.cleaned_data)
