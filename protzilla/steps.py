e4b0a1ff749d971932eef2d019b577e5893b07be
henning <henning.gaertner@student.hpi.de>
2024-04-16 13:44:24 +0200

    Fix writing df to yaml bug

--- protzilla/steps.py
+++ protzilla/steps.py
@@ -29,10 +29,9 @@ class Step:
 
         # validate the inputs for the step
         self.validate_inputs(self.parameter_names)
-        inputs = self.get_input_dataframe(steps, self.inputs)
 
         # calculate the step
-        output_dict = self.method(**inputs)
+        output_dict = self.method(**self.get_input_dataframe(steps, self.inputs))
 
         # store the output and messages
         messages = output_dict.pop("messages", [])
