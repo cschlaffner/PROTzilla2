8cd326853c2d2ef56af0d4995bce603febc2ada9
henning <henning.gaertner@student.hpi.de>
2024-04-23 19:30:46 +0200

    Add some documentation and type hints

--- protzilla/run_v2.py
+++ protzilla/run_v2.py
@@ -4,7 +4,7 @@ import logging
 import threading
 
 import protzilla.constants.paths as paths
-from protzilla.steps import Plots, Step, StepManager
+from protzilla.steps import Messages, Output, Plots, Step, StepManager
 
 
 def get_available_run_names() -> list[str]:
@@ -17,6 +17,10 @@ def get_available_run_names() -> list[str]:
 
 class Run:
     def auto_save(func):
+        """
+        Decorator to automatically save the run in the background after the function is called.
+        """
+
         def wrapper(self, *args, **kwargs):
             result = func(self, *args, **kwargs)
             thread = threading.Thread(target=self._run_write)
@@ -55,56 +59,54 @@ class Run:
         return f"Run({self.run_name}) with {len(self.steps.all_steps)} steps."
 
     @auto_save
-    def _run_read(self):
+    def _run_read(self) -> None:
         self.steps = self.disk_operator.read_run()
 
-    def _run_write(self):
+    def _run_write(self) -> None:
         self.disk_operator.write_run(self.steps)
 
     @auto_save
-    def _workflow_read(self):
+    def _workflow_read(self) -> None:
         self.steps = self.disk_operator.read_workflow()
 
-    def _workflow_export(self, workflow_name: str | None = None):
+    def _workflow_export(self, workflow_name: str | None = None) -> None:
         if workflow_name:
             self.workflow_name = workflow_name
         self.disk_operator.export_workflow(self.steps, self.workflow_name)
 
     @auto_save
-    def step_add(self, step: Step, step_index: int | None = None):
-        self.steps.add_step(step, step_index)
+    def step_add(self, step: Step, step_index: int | None = None) -> None:
+        self.steps.add_step(step)
 
     @auto_save
-    def step_remove(self, step: Step | None = None, step_index: int | None = None):
+    def step_remove(
+        self, step: Step | None = None, step_index: int | None = None
+    ) -> None:
         self.steps.remove_step(step=step, step_index=step_index)
 
     @auto_save
-    def step_calculate(self, inputs: dict | None = None):
+    def step_calculate(self, inputs: dict | None = None) -> None:
         self.steps.current_step.calculate(self.steps, inputs)
 
     @auto_save
-    def step_plot(self):
+    def step_plot(self) -> None:
         self.steps.current_step.plot()
 
-    def step_next(self):
+    def step_next(self) -> None:
         self.steps.next_step()
 
-    def step_previous(self):
+    def step_previous(self) -> None:
         if self.steps.current_step_index > 0:
             self.steps.current_step_index -= 1
         else:
             logging.warning("Cannot go back from the first step")
 
     @auto_save
-    def step_change_method(self, new_method: str):
+    def step_change_method(self, new_method: str) -> None:
         self.steps.change_method(new_method)
 
     @property
-    def is_at_last_step(self):
-        return self.steps.is_at_last_step
-
-    @property
-    def current_messages(self):
+    def current_messages(self) -> Messages:
         return self.steps.current_step.messages
 
     @property
@@ -112,9 +114,9 @@ class Run:
         return self.steps.current_step.plots
 
     @property
-    def current_outputs(self):
+    def current_outputs(self) -> Output:
         return self.steps.current_step.output
 
     @property
-    def current_step(self):
+    def current_step(self) -> Step | None:
         return self.steps.current_step
