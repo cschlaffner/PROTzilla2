27be2f85da1e3584a32d7ac070fdf851cc055c45
janni.roebbecke <janni.roebbecke@student.hpi.uni-potsdam.de>
2024-04-23 11:58:52 +0200

    adapt data_preprocessing test to refactoring
    Co-authored-by: janni.roebbecke <janni.roebbecke@student.hpi.uni-potsdam.de>

--- tests/protzilla/data_preprocessing/test_outlier_detection.py
+++ tests/protzilla/data_preprocessing/test_outlier_detection.py
@@ -11,6 +11,7 @@ from protzilla.data_preprocessing.outlier_detection import (
     by_pca_plot,
 )
 
+
 # TODO #21: implement actual tests for outlier detection
 
 
@@ -59,22 +60,37 @@ def outlier_detection_df_with_nan():
 
 
 def test_outlier_detection_with_isolation_forest(show_figures, outlier_detection_df):
-    result_df, dropouts = by_isolation_forest(outlier_detection_df, 50, -1)
-    fig = by_isolation_forest_plot(outlier_detection_df, result_df, dropouts)[0]
+    method_inputs = {
+        "protein_df": outlier_detection_df,
+        "n_estimators": 50,
+        "n_jobs": -1,
+    }
+    method_outputs = by_isolation_forest(**method_inputs)
+    fig = by_isolation_forest_plot(method_inputs, method_outputs)[0]
     if show_figures:
         fig.show()
 
 
 def test_outlier_detection_with_isolation_forest_and_nan(outlier_detection_df_with_nan):
-    _, current_out = by_isolation_forest(outlier_detection_df_with_nan, 50, -1)
+    method_inputs = {
+        "protein_df": outlier_detection_df_with_nan,
+        "n_estimators": 50,
+        "n_jobs": -1,
+    }
+    methtod_outputs = by_isolation_forest(**method_inputs)
 
-    assert "messages" in current_out
-    assert "NaN values" in current_out["messages"][0]["msg"]
+    assert "messages" in methtod_outputs
+    assert "NaN values" in methtod_outputs["messages"][0]["msg"]
 
 
 def test_outlier_detection_by_local_outlier_factor(show_figures, outlier_detection_df):
-    result_df, dropouts = by_local_outlier_factor(outlier_detection_df, 35, -1)
-    fig = by_local_outlier_factor_plot(outlier_detection_df, result_df, dropouts)[0]
+    method_inputs = {
+        "protein_df": outlier_detection_df,
+        "number_of_neighbors": 35,
+        "n_jobs": -1,
+    }
+    method_outputs = by_local_outlier_factor(**method_inputs)
+    fig = by_local_outlier_factor_plot(method_inputs, method_outputs)[0]
     if show_figures:
         fig.show()
 
@@ -82,21 +98,36 @@ def test_outlier_detection_by_local_outlier_factor(show_figures, outlier_detecti
 def test_outlier_detection_by_local_outlier_factor_and_nan(
     outlier_detection_df_with_nan,
 ):
-    _, current_out = by_local_outlier_factor(outlier_detection_df_with_nan, 35, -1)
+    method_inputs = {
+        "protein_df": outlier_detection_df_with_nan,
+        "number_of_neighbors": 35,
+        "n_jobs": -1,
+    }
+    method_outputs = by_local_outlier_factor(**method_inputs)
 
-    assert "messages" in current_out
-    assert "NaN values" in current_out["messages"][0]["msg"]
+    assert "messages" in method_outputs
+    assert "NaN values" in method_outputs["messages"][0]["msg"]
 
 
 def test_outlier_detection_with_pca(show_figures, outlier_detection_df):
-    result_df, dropouts = by_pca(outlier_detection_df, 2, 3)
-    fig = by_pca_plot(outlier_detection_df, result_df, dropouts)[0]
+    method_inputs = {
+        "protein_df": outlier_detection_df,
+        "threshold": 2,
+        "number_of_components": 3,
+    }
+    method_outputs = by_pca(**method_inputs)
+    fig = by_pca_plot(method_inputs, method_outputs)[0]
     if show_figures:
         fig.show()
 
 
 def test_outlier_detection_with_pca_and_nan(outlier_detection_df_with_nan):
-    _, current_out = by_pca(outlier_detection_df_with_nan, 2, 3)
+    method_inputs = {
+        "protein_df": outlier_detection_df_with_nan,
+        "threshold": 2,
+        "number_of_components": 3,
+    }
+    method_outputs = by_pca(**method_inputs)
 
-    assert "messages" in current_out
-    assert "NaN values" in current_out["messages"][0]["msg"]
+    assert "messages" in method_outputs
+    assert "NaN values" in method_outputs["messages"][0]["msg"]
