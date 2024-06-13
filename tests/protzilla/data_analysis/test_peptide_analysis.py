abb974b04103a9286a51191cbacabb9e5dfe5b22
janni.roebbecke <janni.roebbecke@student.hpi.uni-potsdam.de>
2024-06-13 19:58:05 +0200

    complete merge (fix testes after merge)

--- tests/protzilla/data_analysis/test_peptide_analysis.py
+++ tests/protzilla/data_analysis/test_peptide_analysis.py
@@ -2,14 +2,13 @@ import pytest
 
 from protzilla.data_analysis.ptm_analysis import filter_peptides_of_protein, ptms_per_sample, \
     ptms_per_protein_and_sample
-from tests.protzilla.data_preprocessing.test_peptide_preprocessing import peptides_df, evidence_peptide_df
 
 
 @pytest.mark.parametrize("df_num", [0, 1])
 def test_filter_peptides_of_protein(peptides_df, evidence_peptide_df, df_num):
     peptide_df = [peptides_df, evidence_peptide_df][df_num]
 
-    filtered_peptides_df = filter_peptides_of_protein(peptide_df, "Protein2")["peptide_df"]
+    filtered_peptides_df = filter_peptides_of_protein(peptide_df, ["Protein2"])["peptide_df"]
 
     assert len(filtered_peptides_df) == 6
     assert filtered_peptides_df["Sequence"].tolist() == [
@@ -21,7 +20,7 @@ def test_filter_peptides_of_protein(peptides_df, evidence_peptide_df, df_num):
 def test_ptms_per_sampel(evidence_peptide_df):
     ptm_df = ptms_per_sample(evidence_peptide_df)["ptm_df"]
 
-    assert ptm_df.columns.tolist() == ["Sample", "Unmodified", "Acetyl (Protein N-term)", "Oxidation (M)"]
+    assert ptm_df.columns.tolist() == ["Sample", "Acetyl (Protein N-term)", "Oxidation (M)", "Unmodified"]
     assert ptm_df["Sample"].tolist() == ["Sample1", "Sample2", "Sample3", "Sample4"]
     assert ptm_df["Unmodified"].tolist() == [7, 4, 5, 4]
     assert ptm_df["Acetyl (Protein N-term)"].tolist() == [2, 1, 0, 0]
