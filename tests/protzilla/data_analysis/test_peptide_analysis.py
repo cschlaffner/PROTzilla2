962d6806ae74ee8ae64bff23c52e4ee9d607ce8d
janni.roebbecke <janni.roebbecke@student.hpi.uni-potsdam.de>
2024-06-13 15:00:44 +0200

    added test for ptms_per_protein_and_sample

--- tests/protzilla/data_analysis/test_peptide_analysis.py
+++ tests/protzilla/data_analysis/test_peptide_analysis.py
@@ -1,6 +1,7 @@
 import pytest
 
-from protzilla.data_analysis.ptm_analysis import filter_peptides_of_protein, ptms_per_sample
+from protzilla.data_analysis.ptm_analysis import filter_peptides_of_protein, ptms_per_sample, \
+    ptms_per_protein_and_sample
 from tests.protzilla.data_preprocessing.test_peptide_preprocessing import peptides_df, evidence_peptide_df
 
 
@@ -25,3 +26,20 @@ def test_ptms_per_sampel(evidence_peptide_df):
     assert ptm_df["Unmodified"].tolist() == [7, 4, 5, 4]
     assert ptm_df["Acetyl (Protein N-term)"].tolist() == [2, 1, 0, 0]
     assert ptm_df["Oxidation (M)"].tolist() == [1, 0, 0, 0]
+
+
+def test_ptms_per_protein_and_sample(evidence_peptide_df):
+    ptm_df = ptms_per_protein_and_sample(evidence_peptide_df)["ptm_df"]
+
+    assert ptm_df.columns.tolist() == ["Sample", "Protein1", "Protein2", "Protein3", "Protein4", "Protein5"]
+    assert ptm_df["Sample"].tolist() == ["Sample1", "Sample2", "Sample3", "Sample4"]
+    assert (ptm_df["Protein1"].tolist() ==
+            ["(1) Unmodified, ", "(1) Acetyl (Protein N-term), ", "(1) Unmodified, ", "(1) Unmodified, "])
+    assert (ptm_df["Protein2"].tolist() ==
+            ["(2) Acetyl (Protein N-term), (1) Oxidation (M), (1) Unmodified, ", "(1) Unmodified, ", "(1) Unmodified, ", "(1) Unmodified, "])
+    assert (ptm_df["Protein3"].tolist() ==
+            ["(3) Unmodified, ", "(1) Unmodified, ", "(1) Unmodified, ", "(1) Unmodified, "])
+    assert (ptm_df["Protein4"].tolist() ==
+            ["(1) Unmodified, ", "(1) Unmodified, ", "(1) Unmodified, ", "(1) Unmodified, "])
+    assert (ptm_df["Protein5"].tolist() ==
+            ["(1) Unmodified, ", "(1) Unmodified, ", "(1) Unmodified, ", ""])
