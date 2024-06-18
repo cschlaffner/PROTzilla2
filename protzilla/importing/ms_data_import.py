754c820e208c6ec90d3b710e228d85a9d7035d94
janni.roebbecke <janni.roebbecke@student.hpi.uni-potsdam.de>
2024-06-18 16:52:35 +0200

    maintain order of protein ids in protein group during transform and clean

--- protzilla/importing/ms_data_import.py
+++ protzilla/importing/ms_data_import.py
@@ -1,11 +1,13 @@
 import logging
 import re
+import traceback
 from collections import defaultdict
 
 import numpy as np
 import pandas as pd
 
 from protzilla.data_integration.database_query import biomart_query
+from protzilla.utilities import format_trace
 
 
 def max_quant_import(file_path: str, intensity_name: str, map_to_uniprot=False) -> dict:
@@ -34,7 +36,7 @@ def max_quant_import(file_path: str, intensity_name: str, map_to_uniprot=False) 
 
     except Exception as e:
         msg = f"An error occurred while reading the file: {e.__class__.__name__} {e}. Please provide a valid Max Quant file."
-        return dict(messages=[dict(level=logging.ERROR, msg=msg)])
+        return dict(messages=[dict(level=logging.ERROR, msg=msg, trace=format_trace(traceback.format_exception(e)))])
 
 
 def ms_fragger_import(
@@ -86,7 +88,7 @@ def ms_fragger_import(
         return transform_and_clean(intensity_df, intensity_name, map_to_uniprot)
     except Exception as e:
         msg = f"An error occurred while reading the file: {e.__class__.__name__} {e}. Please provide a valid MS Fragger file."
-        return dict(None, messages=[dict(level=logging.ERROR, msg=msg)])
+        return dict(messages=[dict(level=logging.ERROR, msg=msg, trace=format_trace(traceback.format_exception(e)))])
 
 
 def diann_import(file_path, map_to_uniprot=False) -> dict:
@@ -116,7 +118,7 @@ def diann_import(file_path, map_to_uniprot=False) -> dict:
         return transform_and_clean(intensity_df, intensity_name, map_to_uniprot)
     except Exception as e:
         msg = f"An error occurred while reading the file: {e.__class__.__name__} {e}. Please provide a valid DIA-NN MS file."
-        return dict(None, messages=[dict(level=logging.ERROR, msg=msg)])
+        return dict(messages=[dict(level=logging.ERROR, msg=msg, trace=format_trace(traceback.format_exception(e)))])
 
 
 def transform_and_clean(
@@ -146,7 +148,6 @@ def transform_and_clean(
     new_groups, filtered_proteins = clean_protein_groups(
         df["Protein ID"].tolist(), map_to_uniprot
     )
-    new_groups = pd.Series(new_groups).str.split(";").str[0]
     df = df.assign(**{"Protein ID": new_groups})
 
     has_valid_protein_id = df["Protein ID"].map(bool)
@@ -193,18 +194,20 @@ def clean_protein_groups(protein_groups, map_to_uniprot=True):
     # go through all groups and find the valid proteins
     # non uniprot ids are put into extracted_ids, so they can be mapped
     for group in protein_groups:
-        found_in_group = set()
+        if "A0A0C4DH41" in group:
+            print(group)
+        found_in_group = []
         for protein_id in group.split(";"):
             if not protein_id.startswith("ENSP") and (
                 match := uniprot_regex.search(protein_id)
             ):
-                found_in_group.add(match.group(0))
+                found_in_group.append(match.group(0))
                 continue
             for identifier, pattern in regex.items():
                 if match := pattern.search(protein_id):
                     found_id = match.group(0)
                     extracted_ids[identifier].add(found_id)
-                    found_in_group.add(found_id)
+                    found_in_group.append(found_id)
                     break  # can only match one regex
             else:
                 removed_protein_ids.append(protein_id)
@@ -215,16 +218,17 @@ def clean_protein_groups(protein_groups, map_to_uniprot=True):
     new_groups = []
 
     for group in found_ids_per_group:
-        all_ids_of_group = set()
+        all_ids_of_group = []
         for old_id in group:
             if uniprot_regex.search(old_id):
-                all_ids_of_group.add(old_id)
+                all_ids_of_group.append(old_id)
             elif map_to_uniprot:
-                new_ids = id_to_uniprot.get(old_id, [])
-                all_ids_of_group.update(new_ids)
+                new_ids = pd.Series(id_to_uniprot.get(old_id, []))
+                new_ids = new_ids[new_ids.str.notin(all_ids_of_group)]
+                all_ids_of_group.extend(new_ids)
             else:
-                all_ids_of_group.add(old_id)
-        new_groups.append(";".join(sorted(all_ids_of_group)))
+                all_ids_of_group.append(old_id)
+        new_groups.append(all_ids_of_group[0] if all_ids_of_group else '')
     return new_groups, removed_protein_ids
 
 
