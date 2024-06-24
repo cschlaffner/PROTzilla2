685fb8022a7e5c6c69a639c615b65f53691ba590
JanniRoebbecke <50395935+JanniRoebbecke@users.noreply.github.com>
2024-06-24 21:02:46 +0200

    Single protein (#509)
    * single protein is selected for all methods of ms data import and peptide import
    
    * maintain order of protein ids in protein group during transform and clean
    
    * make the aggregation method optional
    
    * fix circular import
    
    * fix key word arguments for median and mean
    
    * fix tests
    
    * fix issue with empty choice ("-------") in aggregation_method multiple choice field
    
    * implements suggested changes
    
    ---------
    
    Co-authored-by: janni.roebbecke <janni.roebbecke@student.hpi.uni-potsdam.de>

--- protzilla/importing/ms_data_import.py
+++ protzilla/importing/ms_data_import.py
@@ -1,14 +1,18 @@
 import logging
 import re
+import traceback
 from collections import defaultdict
 
 import numpy as np
 import pandas as pd
 
 from protzilla.data_integration.database_query import biomart_query
+from protzilla.utilities import format_trace
 
 
-def max_quant_import(file_path: str, intensity_name: str, map_to_uniprot=False) -> dict:
+def max_quant_import(
+    file_path: str, intensity_name: str, map_to_uniprot=False, aggregation_method: str ="Sum"
+) -> dict:
     assert intensity_name in ["Intensity", "iBAQ", "LFQ intensity"]
     try:
         df = pd.read_csv(
@@ -18,7 +22,7 @@ def max_quant_import(file_path: str, intensity_name: str, map_to_uniprot=False) 
             na_values=["", 0],
             keep_default_na=True,
         )
-        protein_groups = df["Protein IDs"]
+        protein_groups = df["Majority protein IDs"]
         intensity_df = df.filter(regex=f"^{intensity_name} ", axis=1)
         intensity_df = intensity_df.filter(regex=r"^(?!.*peptides).*$", axis=1)
 
@@ -30,15 +34,15 @@ def max_quant_import(file_path: str, intensity_name: str, map_to_uniprot=False) 
             c[len(intensity_name) + 1 :] for c in intensity_df.columns
         ]
         intensity_df = intensity_df.assign(**{"Protein ID": protein_groups})
-        return transform_and_clean(intensity_df, intensity_name, map_to_uniprot)
+        return transform_and_clean(intensity_df, intensity_name, map_to_uniprot, aggregation_method)
 
     except Exception as e:
         msg = f"An error occurred while reading the file: {e.__class__.__name__} {e}. Please provide a valid Max Quant file."
-        return dict(messages=[dict(level=logging.ERROR, msg=msg)])
+        return dict(messages=[dict(level=logging.ERROR, msg=msg, trace=format_trace(traceback.format_exception(e)))])
 
 
 def ms_fragger_import(
-    file_path: str, intensity_name: str, map_to_uniprot=False
+    file_path: str, intensity_name: str, map_to_uniprot=False, aggregation_method: str ="Sum"
 ) -> dict:
     assert intensity_name in [
         "Intensity",
@@ -83,13 +87,13 @@ def ms_fragger_import(
         )
         intensity_df = intensity_df.assign(**{"Protein ID": protein_groups})
 
-        return transform_and_clean(intensity_df, intensity_name, map_to_uniprot)
+        return transform_and_clean(intensity_df, intensity_name, map_to_uniprot, aggregation_method)
     except Exception as e:
         msg = f"An error occurred while reading the file: {e.__class__.__name__} {e}. Please provide a valid MS Fragger file."
-        return dict(None, messages=[dict(level=logging.ERROR, msg=msg)])
+        return dict(messages=[dict(level=logging.ERROR, msg=msg, trace=format_trace(traceback.format_exception(e)))])
 
 
-def diann_import(file_path, map_to_uniprot=False) -> dict:
+def diann_import(file_path, map_to_uniprot=False, aggregation_method: str ="Sum") -> dict:
     try:
         df = pd.read_csv(
             file_path,
@@ -113,14 +117,14 @@ def diann_import(file_path, map_to_uniprot=False) -> dict:
 
         intensity_name = "Intensity"
 
-        return transform_and_clean(intensity_df, intensity_name, map_to_uniprot)
+        return transform_and_clean(intensity_df, intensity_name, map_to_uniprot, aggregation_method)
     except Exception as e:
         msg = f"An error occurred while reading the file: {e.__class__.__name__} {e}. Please provide a valid DIA-NN MS file."
-        return dict(None, messages=[dict(level=logging.ERROR, msg=msg)])
+        return dict(messages=[dict(level=logging.ERROR, msg=msg, trace=format_trace(traceback.format_exception(e)))])
 
 
 def transform_and_clean(
-    df: pd.DataFrame, intensity_name: str, map_to_uniprot: bool
+    df: pd.DataFrame, intensity_name: str, map_to_uniprot: bool, aggregation_method: str ="Sum"
 ) -> dict:
     """
     Transforms a dataframe that is read from a file in wide format into long format,
@@ -151,8 +155,10 @@ def transform_and_clean(
     has_valid_protein_id = df["Protein ID"].map(bool)
     df = df[has_valid_protein_id]
 
-    # sum intensities for duplicate protein groups, NaN if all are NaN, sum of numbers otherwise
-    df = df.groupby("Protein ID", as_index=False).sum(min_count=1)
+    # applies the selected aggregation to duplicate protein groups, NaN if all are NaN, aggregation of numbers otherwise
+    aggregation_method = aggregation_method.lower()
+    agg_kwargs = {"sum": {"min_count": 1}, "median": {}, "mean": {}}
+    df = df.groupby("Protein ID", as_index=False).agg(aggregation_method, **agg_kwargs[aggregation_method])
 
     df = df.assign(Gene=lambda _: np.nan)  # add deprecated genes column
 
@@ -192,18 +198,18 @@ def clean_protein_groups(protein_groups, map_to_uniprot=True):
     # go through all groups and find the valid proteins
     # non uniprot ids are put into extracted_ids, so they can be mapped
     for group in protein_groups:
-        found_in_group = set()
+        found_in_group = []
         for protein_id in group.split(";"):
             if not protein_id.startswith("ENSP") and (
                 match := uniprot_regex.search(protein_id)
             ):
-                found_in_group.add(match.group(0))
+                found_in_group.append(match.group(0))
                 continue
             for identifier, pattern in regex.items():
                 if match := pattern.search(protein_id):
                     found_id = match.group(0)
                     extracted_ids[identifier].add(found_id)
-                    found_in_group.add(found_id)
+                    found_in_group.append(found_id)
                     break  # can only match one regex
             else:
                 removed_protein_ids.append(protein_id)
@@ -214,16 +220,17 @@ def clean_protein_groups(protein_groups, map_to_uniprot=True):
     new_groups = []
 
     for group in found_ids_per_group:
-        all_ids_of_group = set()
+        all_ids_of_group = []
         for old_id in group:
             if uniprot_regex.search(old_id):
-                all_ids_of_group.add(old_id)
+                all_ids_of_group.append(old_id)
             elif map_to_uniprot:
-                new_ids = id_to_uniprot.get(old_id, [])
-                all_ids_of_group.update(new_ids)
+                new_ids = pd.Series(id_to_uniprot.get(old_id, []))
+                new_ids = new_ids[~new_ids.isin(all_ids_of_group)]
+                all_ids_of_group.extend(new_ids)
             else:
-                all_ids_of_group.add(old_id)
-        new_groups.append(";".join(sorted(all_ids_of_group)))
+                all_ids_of_group.append(old_id)
+        new_groups.append(all_ids_of_group[0] if all_ids_of_group else '')
     return new_groups, removed_protein_ids
 
 
