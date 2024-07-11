f845ae110a0b2aa9be8bb9fd9c6d9897ec9f8dfd
lucatreide <luca.treide@taktile.com>
2024-07-11 15:51:31 +0200

    remove duplicated code

--- protzilla/importing/peptide_import.py
+++ protzilla/importing/peptide_import.py
@@ -53,17 +53,13 @@ def peptide_import(file_path, intensity_name, map_to_uniprot) -> dict:
 
         molten = molten.rename(columns={"Proteins": "Protein ID"})
         ordered = molten[["Sample", "Protein ID", "Sequence", "Intensity", "PEP"]]
-        ordered.dropna(subset=["Protein ID"], inplace=True)
-        ordered.sort_values(
-            by=["Sample", "Protein ID"], ignore_index=True, inplace=True
-        )
+
     else:
         final_df = df.rename(columns={"Proteins": "Protein ID"})
         ordered = final_df[["Sample", "Protein ID", "Sequence", "Intensity", "PEP"]]
-        ordered.dropna(subset=["Protein ID"], inplace=True)
-        ordered.sort_values(
-            by=["Sample", "Protein ID"], ignore_index=True, inplace=True
-        )
+
+    ordered.dropna(subset=["Protein ID"], inplace=True)
+    ordered.sort_values(by=["Sample", "Protein ID"], ignore_index=True, inplace=True)
 
     new_groups, filtered_proteins = clean_protein_groups(
         ordered["Protein ID"].tolist(), map_to_uniprot
