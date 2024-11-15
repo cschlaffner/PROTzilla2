b78fc154a12a53a7c143925d279dc374c95b2a19
ronjakrg <ronja.krueger@student.hpi.de>
2024-11-15 13:06:23 +0100

    added path constants to export workflow message

--- protzilla/constants/paths.py
+++ protzilla/constants/paths.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 PROJECT_PATH = Path(__file__).resolve().parent.parent.parent
-RUNS_PATH = Path(PROJECT_PATH, "user_data/runs")
-WORKFLOWS_PATH = Path(PROJECT_PATH, "user_data/workflows")
+USER_DATA_PATH = Path(PROJECT_PATH, "user_data")
+RUNS_PATH = USER_DATA_PATH / "runs"
+WORKFLOWS_PATH = USER_DATA_PATH / "workflows"
 EXTERNAL_DATA_PATH = Path(PROJECT_PATH, "user_data/external_data")
 WORKFLOW_META_PATH = Path(PROJECT_PATH, "protzilla/constants/workflow_meta.json")
 UI_PATH = Path(PROJECT_PATH, "ui")
