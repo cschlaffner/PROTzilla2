5c0ddf8674bd813eacf4b5b0f2c3444ee7160834
ronjakrg <ronja.krueger@student.hpi.de>
2024-11-18 00:18:31 +0100

    set plotly_protzilla as default template

--- protzilla/utilities/plot_template.py
+++ protzilla/utilities/plot_template.py
@@ -3,27 +3,28 @@ import plotly.graph_objects as go
 
 from protzilla.constants.colors import PROTZILLA_DISCRETE_COLOR_OUTLIER_SEQUENCE
 
-pio.templates["plotly_protzilla"] = go.layout.Template(
-    layout = {
-        "title": {
-            "font": {
-                "size": 16,
-                "family": "Arial",
-            },
-            "y": 0.98,
-            "x": 0.5,
-            "xanchor": "center",
-            "yanchor": "top",
-        },
+
+layout = go.Layout(
+    title={
         "font": {
-            "size": 14,
+            "size": 16,
             "family": "Arial"
         },
-        "colorway": PROTZILLA_DISCRETE_COLOR_OUTLIER_SEQUENCE,
-        "plot_bgcolor": "white",
-        "yaxis": {
-            "gridcolor": "lightgrey",
-            "zerolinecolor": "lightgrey",
-        },
+        "y": 0.98,
+        "x": 0.5,
+        "xanchor": "center",
+        "yanchor": "top"
+    },
+    font={
+        "size": 14,
+        "family": "Arial"
+    },
+    colorway= PROTZILLA_DISCRETE_COLOR_OUTLIER_SEQUENCE,
+    plot_bgcolor="white",
+    yaxis={
+        "gridcolor": "lightgrey",
+        "zerolinecolor": "lightgrey"
     }
 )
+pio.templates["plotly_protzilla"] = go.layout.Template(layout=layout)
+pio.templates.default = "plotly_protzilla"
