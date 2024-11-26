1d29f3a53cf9ed9c0568f04a97904cb5d6b167c5
gitjannes <jannesk@gmx.net>
2024-11-26 10:36:28 +0100

    Added structure to filter.py

--- ui/runs/filter.py
+++ ui/runs/filter.py
@@ -1,2 +1,10 @@
-def filter_runs(runs, filters) -> None: #to be implemented
+def filter_for_name(runs, search_string) -> list[dict[str, str | list[str]]]:
+    return runs
+
+def filter_for_steps(runs, search_steps) -> list[dict[str, str | list[str]]]:
+    return runs
+
+def filter_runs(runs, filters) -> list[dict[str, str | list[str]]]: #to be implemented
+    runs = filter_for_name(runs, filters.name)
+    runs = filter_for_steps(runs, filters.steps)
     return runs
