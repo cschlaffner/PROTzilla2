f6aa54e51c8d80efb225c251f0a3bf6594772000
maximilianKalff <maximilian.kalff@student.hpi.uni-potsdam.de>
2024-12-12 08:48:55 +0100

    Header fixed, Button hover effect, first run always selected, deselect runs

--- ui/runs/static/runs/index.js
+++ ui/runs/static/runs/index.js
@@ -1,9 +1,46 @@
+// === EVENT LISTENER ===
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Delete Btns
+  const delete_btns = document.querySelectorAll('.delete-btn');
+
+  delete_btns.forEach(button => {
+    const svg = button.querySelector('svg');
+
+    button.addEventListener('mouseover', () => {
+        button.classList.add('btn-red');
+        button.classList.remove('btn-grey');
+        if (svg) svg.setAttribute('fill', 'white');
+    });
+
+    button.addEventListener('mouseout', () => {
+        button.classList.add('btn-grey');
+        button.classList.remove('btn-red');
+        if (svg) svg.setAttribute('fill', 'grey');
+    });
+  });
+
+  // mark first run as selected
+  let first_run = document.getElementById('run-1');
+  first_run.style.backgroundColor = '#e8edf3';
+  first_run.style.borderRadius = '10px';
+
+});
+
+
+// === FUNCTIONS ===
 function selectRun(runName, runId, numberOfRuns){
   for (let i = 1; i <= numberOfRuns; i++) {
       let run = document.getElementById('run-' + i);
       if (i == runId) {
-          run.style.backgroundColor = '#e8edf3';
-          run.style.borderRadius = '10px';
+          if (run.style.backgroundColor == 'rgb(232, 237, 243)') {
+            run.style.backgroundColor = 'white';
+          }
+          else {
+            run.style.backgroundColor = 'rgb(232, 237, 243)';
+            run.style.borderRadius = '10px';
+          }
+          
       }
       else {
           run.style.backgroundColor = 'white';
